bca87181b7eae3c6efe3ce6c50877fd7f94b3d25
kruch <kruhc@seznam.cz>
2006-06-07 11:21:06 +0000

    renders crosshair; able to move to given position

--- src/cz/kruch/track/ui/MapViewer.java
+++ src/cz/kruch/track/ui/MapViewer.java
@@ -5,29 +5,36 @@ package cz.kruch.track.ui;
 
 import cz.kruch.track.maps.Slice;
 import cz.kruch.track.maps.Map;
-import cz.kruch.track.location.Position;
+import cz.kruch.track.util.Logger;
 
 import javax.microedition.lcdui.Graphics;
 import javax.microedition.lcdui.Canvas;
+import javax.microedition.lcdui.Image;
 import javax.microedition.lcdui.game.Sprite;
 import java.util.Vector;
 import java.util.Enumeration;
+import java.io.IOException;
 
 final class MapViewer {
-    private static final String COMPONENT_NAME = "MapViewer";
+    private static final Logger log = new Logger("Map");
 
     private int gx, gy;
     private int width, height;
     private int x, y;
+    private int chx, chy;
 
+    private Image crosshair;
     private Map map;
     private Vector slices = new Vector();
 
-    public MapViewer(int gx, int gy, int width, int height) {
+    public MapViewer(int gx, int gy, int width, int height) throws IOException {
         this.gx = gx;
         this.gy = gy;
         this.width = width;
         this.height = height;
+        this.crosshair = Image.createImage("/resources/crosshair.png");
+        this.chx = (width - crosshair.getWidth()) / 2;
+        this.chy = (height - crosshair.getHeight()) / 2;
     }
 
     public void setMap(Map map) {
@@ -35,7 +42,44 @@ final class MapViewer {
     }
 
     public Position getPosition() {
-        return new Position(x, y);
+        return new Position(x + width / 2, y + height / 2);
+    }
+
+    // TODO better name - x,y is desired position of crosshair!
+    public boolean move(int x, int y) {
+//        System.out.println("move, current position " + this.x + "," + this.y);
+
+        boolean dirty = false;
+        int direction = -1;
+
+        int dx = x - (width / 2) - this.x ;
+        if (dx > 0) {
+            direction = Canvas.RIGHT;
+        } else {
+            direction = Canvas.LEFT;
+        }
+
+        for (int i = 0; i < Math.abs(dx); i++) {
+            dirty |= scroll(direction);
+        }
+
+        int dy = y - (height / 2) - this.y;
+        if (dy > 0) {
+            direction = Canvas.DOWN;
+        } else {
+            direction = Canvas.UP;
+        }
+
+        for (int i = 0; i < Math.abs(dy); i++) {
+            dirty |= scroll(direction);
+        }
+
+        chx = x - this.x - crosshair.getWidth() / 2;
+        chy = y - this.y - crosshair.getHeight() / 2;
+
+//        if (dirty) System.out.println("move made, current position " + this.x + "," + this.y + "; dirty = " + dirty + "; crosshair requested at " + x + "-" + y + " -> screen position at " + chx + "-" + chy);
+
+        return dirty;
     }
 
     public boolean scroll(int direction) {
@@ -74,9 +118,6 @@ final class MapViewer {
                 throw new IllegalArgumentException("Unknown direction");
         }
 
-//        if (!dirty) System.out.println("no move");
-//        System.out.println("x = " + x + ", y = " + y);
-
         return dirty;
     }
 
@@ -88,6 +129,9 @@ final class MapViewer {
         for (Enumeration e = slices.elements(); e.hasMoreElements(); ) {
             drawSlice(graphics, (Slice) e.nextElement());
         }
+
+        // paint crosshair
+        graphics.drawImage(crosshair, chx, chy, Graphics.TOP | Graphics.LEFT);
     }
 
     private void drawSlice(Graphics graphics, Slice slice) {
@@ -133,7 +177,7 @@ final class MapViewer {
 
     public boolean ensureSlices() {
         if (map == null) {
-            System.err.println(COMPONENT_NAME + " [error] no map");
+            log.error("no map");
             throw new IllegalStateException("No map");
         }
 
@@ -150,7 +194,7 @@ final class MapViewer {
             Slice slice = (Slice) e.nextElement();
             if (!collection.contains(slice)) {
                 // debug
-                System.out.println(COMPONENT_NAME + " [debug] release map image in " + slice);
+                log.debug("release map image in " + slice);
 
                 // to be freed
                 garbage.addElement(slice);

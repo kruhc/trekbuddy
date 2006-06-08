294d9dff979f4e3d4c50c1a7b061dba264c27692
kruch <kruhc@seznam.cz>
2006-06-08 17:26:26 +0000

    start/stop methods added

--- src/api/location/LocationProvider.java
+++ src/api/location/LocationProvider.java
@@ -3,10 +3,15 @@
 
 package api.location;
 
+import java.io.IOException;
+
 public abstract class LocationProvider {
     public static final int AVAILABLE = 1;
     public static final int TEMPORARILY_UNAVAILABLE = 2;
     public static final int OUT_OF_SERVICE = 3;
+
+    public abstract void start() throws IOException;
+    public abstract void stop() throws IOException;
 
     public abstract void setLocationListener(LocationListener listener,
                                              int interval,

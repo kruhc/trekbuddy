8f32cba30de2806fe8365d9b5447e51b9f5133c5
kruch <kruhc@seznam.cz>
2006-06-09 07:40:02 +0000

    showing provider status

--- src/cz/kruch/track/ui/OSD.java
+++ src/cz/kruch/track/ui/OSD.java
@@ -3,19 +3,33 @@
 
 package cz.kruch.track.ui;
 
+import api.location.LocationProvider;
+
 import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.Image;
+import java.io.IOException;
 
 class OSD extends Bar {
     private static final String NO_INFO = "Lon: ? Lat: ?";
 
-    public OSD(int gx, int gy, int width, int height) {
+    private Image providerAvailable;
+    private Image providerUnavailable;
+    private Image providerOutOfService;
+    private int providerStatus = LocationProvider.OUT_OF_SERVICE;
+    private int semaforX, semaforY;
+
+    public OSD(int gx, int gy, int width, int height) throws IOException {
         super(gx, gy, width, height);
+        this.providerAvailable = Image.createImage("/resources/s_green.png");
+        this.providerUnavailable = Image.createImage("/resources/s_orange.png");
+        this.providerOutOfService = Image.createImage("/resources/s_red.png");
+        this.semaforX = this.width - this.providerAvailable.getWidth() - BORDER;
+        this.semaforY = Math.abs((this.font.getHeight() - this.providerAvailable.getHeight())) / 2;
     }
 
     public void render(Graphics graphics) {
         if (info == null) {
             info = NO_INFO;
-
         }
 
         // draw position
@@ -27,5 +41,22 @@ class OSD extends Bar {
         }
         graphics.setFont(font);
         graphics.drawString(info, gx, gy, Graphics.TOP | Graphics.LEFT);
+
+        // draw provider status
+        switch (providerStatus) {
+            case LocationProvider.AVAILABLE:
+                graphics.drawImage(providerAvailable, semaforX, semaforY, Graphics.TOP | Graphics.LEFT);
+                break;
+            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                graphics.drawImage(providerUnavailable, semaforX, semaforY, Graphics.TOP | Graphics.LEFT);
+                break;
+            case LocationProvider.OUT_OF_SERVICE:
+                graphics.drawImage(providerOutOfService,  semaforX, semaforY, Graphics.TOP | Graphics.LEFT);
+                break;
+        }
+    }
+
+    public void setProviderStatus(int providerStatus) {
+        this.providerStatus = providerStatus;
     }
 }

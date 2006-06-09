580752b108908a25f77ae2528a8230de996e5ba1
kruch <kruhc@seznam.cz>
2006-06-09 07:43:47 +0000

    enhanced for better processing

--- src/api/location/LocationProvider.java
+++ src/api/location/LocationProvider.java
@@ -3,18 +3,28 @@
 
 package api.location;
 
-import java.io.IOException;
-
 public abstract class LocationProvider {
     public static final int AVAILABLE = 1;
     public static final int TEMPORARILY_UNAVAILABLE = 2;
     public static final int OUT_OF_SERVICE = 3;
 
-    public abstract void start() throws IOException;
-    public abstract void stop() throws IOException;
+    private String name;
+
+    protected LocationProvider(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void start() throws LocationException;
+    public abstract void stop() throws LocationException;
 
     public abstract void setLocationListener(LocationListener listener,
                                              int interval,
                                              int timeout,
                                              int maxAge);
+
+    public abstract Object getImpl();
 }

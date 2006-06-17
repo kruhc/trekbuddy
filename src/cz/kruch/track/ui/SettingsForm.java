c5ff9d925f09c1d0f76dd974cf85c97a7382740a
kruch <kruhc@seznam.cz>
2006-06-17 23:42:11 +0000

    extends Form; provider-specific option showing; tracklog options

--- src/cz/kruch/track/ui/SettingsForm.java
+++ src/cz/kruch/track/ui/SettingsForm.java
@@ -13,8 +13,10 @@ import javax.microedition.lcdui.ChoiceGroup;
 import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.TextField;
+import javax.microedition.lcdui.ItemStateListener;
+import javax.microedition.lcdui.Item;
 
-public class SettingsForm implements CommandListener {
+public class SettingsForm extends Form implements CommandListener {
     private static final int MAX_URL_LENGTH = 128;
 
     private Display display;
@@ -22,23 +24,24 @@ public class SettingsForm implements CommandListener {
 
     private TextField fieldMapPath;
     private ChoiceGroup choiceProvider;
-    private ChoiceGroup choiceDesktop;
+    private ChoiceGroup choiceTracklogs;
+    private TextField fieldTracklogsDir;
     private TextField fieldSimulatorPath;
     private TextField fieldSimulatorDelay;
+    private ChoiceGroup choiceDesktop;
 
     public SettingsForm(Display display) {
+        super("Settings");
         this.display = display;
         this.previous = display.getCurrent();
     }
 
     public void show() {
-        Form form = new Form("Settings");
-
-        // map path field
+        // default map path field
         fieldMapPath = new TextField("Default Map", Config.getSafeInstance().getMapPath(), MAX_URL_LENGTH, TextField.URL);
-        form.append(fieldMapPath);
+        append(fieldMapPath);
 
-        // location provider radioboxes
+        // location provider choice
         choiceProvider = new ChoiceGroup("Location Provider", ChoiceGroup.EXCLUSIVE);
         String[] providers = Config.getSafeInstance().getLocationProviders();
         for (int N = providers.length, i = 0; i < N; i++) {
@@ -48,27 +51,42 @@ public class SettingsForm implements CommandListener {
                 choiceProvider.setSelectedIndex(idx, true);
             }
         }
-        form.append(choiceProvider);
+        append(choiceProvider);
+
+        // tracklogs
+        choiceTracklogs = new ChoiceGroup("Tracklogs", ChoiceGroup.MULTIPLE);
+        choiceTracklogs.setSelectedIndex(choiceTracklogs.append("enabled", null), Config.getSafeInstance().isTracklogsOn());
+        fieldTracklogsDir = new TextField("Tracklogs Dir", Config.getSafeInstance().getTracklogsDir(), MAX_URL_LENGTH, TextField.URL);
+//        append(choiceTracklogs);
+//        append(fieldTracklogsDir);
+
+        // simulator
+        fieldSimulatorPath = new TextField("Simulator File", Config.getSafeInstance().getSimulatorPath(), MAX_URL_LENGTH, TextField.URL);
+        fieldSimulatorDelay = new TextField("Simulator Delay", Integer.toString(Config.getSafeInstance().getSimulatorDelay()), 8, TextField.NUMERIC);
+//        append(fieldSimulatorPath);
+//        append(fieldSimulatorDelay);
 
         // desktop settings
         choiceDesktop = new ChoiceGroup("Desktop", ChoiceGroup.MULTIPLE);
         choiceDesktop.setSelectedIndex(choiceDesktop.append("fullscreen", null), Config.getSafeInstance().isFullscreen());
-        form.append(choiceDesktop);
+        append(choiceDesktop);
 
-        // simulator path field
-        fieldSimulatorPath = new TextField("Simulator File", Config.getSafeInstance().getSimulatorPath(), 32, TextField.URL);
-        fieldSimulatorDelay = new TextField("Simulator Delay", Integer.toString(Config.getSafeInstance().getSimulatorDelay()), 8, TextField.NUMERIC);
-        form.append(fieldSimulatorPath);
-        form.append(fieldSimulatorDelay);
+        // show provider specific options
+        showProviderOptions();
 
         // add command and handling
-        form.addCommand(new Command("Back", Command.BACK, 1));
-        form.addCommand(new Command("Apply", Command.SCREEN, 2));
-        form.addCommand(new Command("Save", Command.SCREEN, 2));
-        form.setCommandListener(this);
+        addCommand(new Command("Close", Command.BACK, 1));
+        addCommand(new Command("Apply", Command.SCREEN, 1));
+        addCommand(new Command("Save", Command.SCREEN, 2));
+        setCommandListener(this);
+        setItemStateListener(new ItemStateListener() {
+            public void itemStateChanged(Item item) {
+                showProviderOptions();
+            }
+        });
 
         // show
-        display.setCurrent(form);
+        display.setCurrent(this);
     }
 
     public void commandAction(Command command, Displayable displayable) {
@@ -82,9 +100,11 @@ public class SettingsForm implements CommandListener {
             Config config = Config.getSafeInstance();
             config.setMapPath(fieldMapPath.getString());
             config.setLocationProvider(choiceProvider.getString(choiceProvider.getSelectedIndex()));
-            config.setFullscreen(choiceDesktop.isSelected(0));
+            config.setTracklogsOn(choiceTracklogs.isSelected(0));
+            config.setTracklogsDir(fieldTracklogsDir.getString());
             config.setSimulatorPath(fieldSimulatorPath.getString());
             config.setSimulatorDelay(Integer.parseInt(fieldSimulatorDelay.getString()));
+            config.setFullscreen(choiceDesktop.isSelected(0));
             if ("Save".equals(command.getLabel())) {
                 try {
                     config.update();
@@ -106,5 +126,35 @@ public class SettingsForm implements CommandListener {
 
         // notify that we are done
         (new Desktop.Event(Desktop.Event.EVENT_CONFIGURATION_CHANGED, changed ? Boolean.TRUE : Boolean.FALSE, null)).fire();
+    }
+
+    private void showProviderOptions() {
+        for (int i = 0; i < size(); i++) {
+            Item item = get(i);
+            if (!(fieldMapPath == item || choiceProvider == item || choiceDesktop == item)) {
+                delete(i);
+                i = 0;
+            }
+        }
+
+        int insertAt = 0;
+        for (int N = size(), i = 0; i < N; i++) {
+            Item item = get(i);
+            if (choiceDesktop == item) {
+                insertAt = i;
+                break;
+            }
+        }
+
+        String provider = choiceProvider.getString(choiceProvider.getSelectedIndex());
+        if (Config.LOCATION_PROVIDER_SIMULATOR.equals(provider)) {
+            insert(insertAt, fieldSimulatorDelay);
+            insert(insertAt, fieldSimulatorPath);
+        } else if (Config.LOCATION_PROVIDER_JSR82.equals(provider)) {
+            insert(insertAt, fieldTracklogsDir);
+            insert(insertAt, choiceTracklogs);
+        } else if (Config.LOCATION_PROVIDER_JSR179.equals(provider)) {
+            // nothing to show
+        }
     }
 }

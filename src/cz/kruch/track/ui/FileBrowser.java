bd73c8bd2c9a4cb4450fdb43662b24f92a62e15c
kruch <kruhc@seznam.cz>
2006-06-17 23:44:22 +0000

    separate thead fs traversing; extends List

--- src/cz/kruch/track/ui/FileBrowser.java
+++ src/cz/kruch/track/ui/FileBrowser.java
@@ -3,6 +3,8 @@
 
 package cz.kruch.track.ui;
 
+import cz.kruch.track.util.Logger;
+
 import javax.microedition.io.file.FileSystemRegistry;
 import javax.microedition.io.file.FileConnection;
 import javax.microedition.io.Connector;
@@ -12,117 +14,149 @@ import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Displayable;
 import java.util.Enumeration;
-import java.util.EmptyStackException;
 import java.io.IOException;
 
-public class FileBrowser implements CommandListener {
+public class FileBrowser extends List implements CommandListener {
+    private static final Logger log = new Logger("FileBrowser");
     private Display display;
     private Displayable previous;
 
-    private List list;
     private Command cmdCancel;
     private Command cmdBack;
 
-    private FileConnection fc;
-    private String selection;
-    private int depth;
+    private volatile FileConnection fc;
+    private volatile String path;
+    private volatile String selection;
+    private volatile int depth = 0;
+
+    private Thread scanner;
 
     public FileBrowser(Display display) {
+        super("FileBrowser", List.IMPLICIT);
         this.display = display;
         this.previous = display.getCurrent();
+        this.cmdCancel = new Command("Cancel", Command.CANCEL, 1);
+        this.cmdBack = new Command("Back", Command.BACK, 1);
+        setCommandListener(this);
+        display.setCurrent(this);
+    }
+
+    public void show() {
+        browse(ShowRoots);
     }
 
     public String getSelection() {
         return selection;
     }
 
-    public void browse() {
-        Enumeration roots = FileSystemRegistry.listRoots();
-        if (roots.hasMoreElements()) {
-            list = new List("FileBrowser", List.IMPLICIT);
-            cmdCancel = new Command("Cancel", Command.CANCEL, 1);
-            cmdBack = new Command("Back", Command.BACK, 1);
-            depth = 0;
-            Desktop.Event.getDisplay().setCurrent(list);
-            show(roots);
-        } else {
-            (new Desktop.Event(Desktop.Event.EVENT_FILE_BROWSER_FINISHED, "No filesystem roots.", new EmptyStackException())).fire();
+    public void browse(Runnable r) {
+        if (scanner != null) {
+            if (log.isEnabled()) log.debug("join scanner");
+            try {
+                scanner.interrupt();
+                scanner.join();
+            } catch (InterruptedException e) {
+            }
         }
+        scanner = new Thread(r);
+        scanner.start();
     }
 
     private void show(Enumeration entries) {
-        list.deleteAll();
-        list.removeCommand(cmdCancel);
-        list.removeCommand(cmdBack);
+        deleteAll();
+        removeCommand(cmdCancel);
+        removeCommand(cmdBack);
         for (Enumeration e = entries; e.hasMoreElements(); ) {
-            list.append((String) e.nextElement(), null);
+            append((String) e.nextElement(), null);
         }
-        list.addCommand(List.SELECT_COMMAND);
-        list.addCommand(depth == 0 ? cmdCancel :cmdBack);
-        list.setCommandListener(this);
-        System.out.println("show depth " + depth);
+        if (size() == 0 && log.isEnabled()) {
+            append("<empty>", null);
+        }
+        addCommand(List.SELECT_COMMAND);
+        addCommand(depth == 0 ? cmdCancel :cmdBack);
     }
 
     public void commandAction(Command command, Displayable displayable) {
-        boolean quit = true;
-        Throwable throwable = null;
-
-        try {
-            if (command == List.SELECT_COMMAND) {
-                depth++;
-                quit = !scan(list.getString(list.getSelectedIndex()));
-            } else if (command.getCommandType() == Command.BACK) {
-                depth--;
-                if (depth > 0) {
-                    quit = !scan("..");
-                } else {
-                    quit = false;
-                    fc.close();
-                    fc = null;
-                    show(FileSystemRegistry.listRoots());
-                }
+        if (command == List.SELECT_COMMAND) {
+            depth++;
+            path = getString(getSelectedIndex());
+            browse(ShowDirectory);
+        } else if (command.getCommandType() == Command.BACK) {
+            depth--;
+            if (depth > 0) {
+                path = "..";
+                browse(ShowDirectory);
+            } else {
+                browse(ShowRoots);
             }
-        } catch (IOException e) {
-            throwable = e;
+        } else {
+            quit(null);
+        }
+    }
+
+    private void quit(Throwable throwable) {
+        // join scanner
+        if (scanner != null) {
+            if (log.isEnabled()) log.debug("join left scanner");
+            try {
+                scanner.interrupt();
+                scanner.join();
+            } catch (InterruptedException e) {
+            }
         }
 
-        if (quit) {
-            // close connection
+        // close connection
+        if (fc != null) {
+            if (log.isEnabled()) log.debug("join left fc");
+            try {
+                fc.close();
+            } catch (IOException e) {
+            }
+        }
+
+        // gc hint
+        fc = null;
+
+        // restore previous screen
+        display.setCurrent(previous);
+
+        // we are done
+        (new Desktop.Event(Desktop.Event.EVENT_FILE_BROWSER_FINISHED, selection, throwable)).fire();
+    }
+
+    private Runnable ShowDirectory = new Runnable() {
+        public void run() {
+            try {
+                if (fc == null) {
+                    fc = (FileConnection) Connector.open("file:///" + path, Connector.READ);
+                } else {
+                    fc.setFileConnection(path);
+                }
+
+                if (fc.isDirectory()) {
+                    show(fc.list("*", false));
+                } else {
+                    selection = fc.getURL();
+                    quit(null);
+                }
+            } catch (IOException e) {
+                quit(e);
+            }
+        }
+    };
+
+    private Runnable ShowRoots = new Runnable() {
+        public void run() {
             if (fc != null) {
+                if (log.isEnabled()) log.debug("close existing fc");
                 try {
                     fc.close();
                 } catch (IOException e) {
                 }
             }
-
-            // gc hint
             fc = null;
-            list = null;
 
-            // restore previous screen
-            display.setCurrent(previous);
-
-            // we are done
-            (new Desktop.Event(Desktop.Event.EVENT_FILE_BROWSER_FINISHED, selection, throwable)).fire();
+            show(FileSystemRegistry.listRoots());
         }
-    }
-
-    private boolean scan(String path) throws IOException {
-        boolean deeper = false;
-
-        if (fc == null) {
-            fc = (FileConnection) Connector.open("file:///" + path, Connector.READ);
-        } else {
-            fc.setFileConnection(path);
-        }
-
-        if (fc.isDirectory()) {
-            show(fc.list("*", false));
-            deeper = true;
-        } else {
-            selection = fc.getURL();
-        }
-
-        return deeper;
-    }
+    };
 }

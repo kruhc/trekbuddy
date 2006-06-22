1b5fb57ab8f817b3e1f78b34943def246b596b06
kruch <kruhc@seznam.cz>
2006-06-22 13:23:22 +0000

    more data parsed

--- src/cz/kruch/track/util/NmeaParser.java
+++ src/cz/kruch/track/util/NmeaParser.java
@@ -5,21 +5,23 @@ package cz.kruch.track.util;
 
 import api.location.QualifiedCoordinates;
 import api.location.LocationException;
+import api.location.Location;
 import cz.kruch.j2se.util.StringTokenizer;
-import cz.kruch.track.location.SimplifiedLocation;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+import java.util.Date;
 
 public class NmeaParser {
-    // component name for logging
-//    private static final String COMPONENT_NAME = "NmeaParser";
+    private static final Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 
-    public static SimplifiedLocation parse(String nmea) throws LocationException {
+    public static Location parse(String nmea) throws LocationException {
         Record record = new Record();
         int index = 0;
         StringTokenizer st = new StringTokenizer(nmea, ",*", true);
         while (st.hasMoreTokens() && (index < 8)) {
             switch (index) {
                 case 0: {
-//                    System.out.println(COMPONENT_NAME + " [debug] checking header");
                     String token = nextToken(st, false);
                     if (!"$GPGGA".equals(token)) {
                         throw new LocationException("Invalid NMEA record");
@@ -27,62 +29,50 @@ public class NmeaParser {
                     index++;
                 } break;
                 case 1: {
-//                    System.out.println(COMPONENT_NAME + " [debug] parsing timestamp");
                     String token = nextToken(st, false);
-                    record.timestamp = System.currentTimeMillis(); // TODO
+                    record.timestamp = parseDate(token);
                     index++;
                 } break;
                 case 2: {
-//                    System.out.println(COMPONENT_NAME + " [debug] parsing latitude");
                     String token = nextToken(st, false);
                     record.lat = Double.parseDouble(token.substring(0, 2)) + Double.parseDouble(token.substring(2)) / 60D;
                     String ns = nextToken(st, false);
-/*
-                    if (!(ns.startsWith("N") || ns.startsWith("S"))) {
-                        return null;
+                    if (ns.startsWith("S")) {
+                        record.lat *= -1;
                     }
-*/
                     index++;
                 } break;
                 case 3: {
-//                    System.out.println(COMPONENT_NAME + " [debug] parsing longtitude");
                     String token = nextToken(st, false);
                     record.lon = Double.parseDouble(token.substring(0, 3)) + Double.parseDouble(token.substring(3)) / 60D;
                     String ew = nextToken(st, false);
-/*
-                    if (!(ew.startsWith("E") || ew.startsWith("W"))) {
-                        return null;
+                    if (ew.startsWith("W")) {
+                        record.lon *= -1;
                     }
-*/
                     index++;
                 } break;
                 case 4: {
-//                    System.out.println(COMPONENT_NAME + " [debug] parsing quality");
                     String token = nextToken(st, false);
-                    record.quality = Integer.parseInt(token);
+                    record.fix = Integer.parseInt(token);
                     index++;
                 } break;
                 case 5: {
-//                    System.out.println(COMPONENT_NAME + " [debug] parsing number of satellites");
                     String token = nextToken(st, false);
-                    record.satellites = Integer.parseInt(token);
+                    record.sat = Integer.parseInt(token);
                     index++;
                 } break;
                 case 6: {
-//                    System.out.println(COMPONENT_NAME + " [debug] parsing dilution");
                     String token = nextToken(st, false);
                     record.dilution = Float.parseFloat(token);
                     index++;
                 } break;
                 case 7: {
-//                    System.out.println(COMPONENT_NAME + " [debug] parsing altitude");
                     String token = nextToken(st, false);
                     record.altitude = Float.parseFloat(token);
-                    String m = nextToken(st, false);
+                    String m = nextToken(st, false); // unused
                     index++;
                 } break;
                 case 8: {
-//                    System.out.println(COMPONENT_NAME + " [debug] parsing geiod height");
                     String token = nextToken(st, false);
                     if (token != null) {
                         record.geoidh = Float.parseFloat(token);
@@ -91,7 +81,6 @@ public class NmeaParser {
                     index++;
                 } break;
                 case 9: {
-//                    System.out.println(COMPONENT_NAME + " [debug] parsing DGPS update time");
                     String token = nextToken(st, false);
                     if (token != null) {
                         record.dgpst = Integer.parseInt(token);
@@ -99,21 +88,19 @@ public class NmeaParser {
                     index++;
                 } break;
                 case 10: {
-//                    System.out.println(COMPONENT_NAME + " [debug] parsing DGPS ID");
                     record.dgpsid = nextToken(st, false);
                     index++;
                 } break;
                 case 11: {
-//                    System.out.println(COMPONENT_NAME + " [debug] parsing checksum");
                     record.checkum = nextToken(st, true);
                     index++;
                 } break;
             }
         }
 
-        QualifiedCoordinates qualifiedCoordinates = new QualifiedCoordinates(record.lat, record.lon, record.altitude);
+        QualifiedCoordinates coordinates = new QualifiedCoordinates(record.lat, record.lon, record.altitude);
 
-        return new SimplifiedLocation(qualifiedCoordinates, record.timestamp);
+        return new Location(coordinates, record.timestamp, record.fix, record.sat);
     }
 
     private static String nextToken(StringTokenizer st, boolean last) {
@@ -128,14 +115,29 @@ public class NmeaParser {
         return s;
     }
 
+    private static long parseDate(String ts) {
+        int tl = Double.valueOf(ts).intValue();
+        calendar.setTime(new Date(System.currentTimeMillis()));
+        int hours = tl / 10000;
+        tl -= hours * 10000;
+        int mins = tl / 100;
+        tl -= mins * 100;
+        int sec = tl;
+        calendar.set(Calendar.HOUR_OF_DAY, hours);
+        calendar.set(Calendar.MINUTE, mins);
+        calendar.set(Calendar.SECOND, sec);
+
+        return calendar.getTime().getTime();
+    }
+
     private static class Record {
         public long timestamp;
         public double lat;
         public double lon;
-        public int quality;
-        public int satellites;
+        public int fix = -1;
+        public int sat = -1;
         public float dilution;
-        public float altitude;
+        public float altitude = -1;
         public float geoidh;
         public int dgpst;
         public String dgpsid;

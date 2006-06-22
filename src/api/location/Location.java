92c2780b405f5a1db32f5dafa25b0ce360dc4900
kruch <kruhc@seznam.cz>
2006-06-22 13:12:49 +0000

    extende to hold more relevant info (replaces SimplifiedLocation)

--- src/api/location/Location.java
+++ src/api/location/Location.java
@@ -3,15 +3,44 @@
 
 package api.location;
 
+import java.util.Date;
+
 public class Location {
-    protected Location() {
+    private QualifiedCoordinates coordinates;
+    private long timestamp;
+    private int fix;
+    private int sat;
+
+    public Location(QualifiedCoordinates coordinates, long timestamp, int fix) {
+        this(coordinates, timestamp, fix, -1);
+    }
+
+    public Location(QualifiedCoordinates coordinates, long timestamp, int fix, int sat) {
+        this.coordinates = coordinates;
+        this.timestamp = timestamp;
+        this.fix = fix;
+        this.sat = sat;
     }
 
     public QualifiedCoordinates getQualifiedCoordinates() {
-        return null;
+        return coordinates;
     }
 
     public long getTimestamp() {
-        return System.currentTimeMillis();
+        return timestamp;
     }
+
+    public int getFix() {
+        return fix;
+    }
+
+    public int getSat() {
+        return sat;
+    }
+
+    // debug
+    public String toString() {
+        return (new Date(timestamp)).toString() + ": " + coordinates.toString() + ";" + fix + ";" + sat;
+    }
+    // ~debug
 }

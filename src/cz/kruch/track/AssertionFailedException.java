7395469b949bc5993f041b2d34bdf43e1924d04b
kruch <kruhc@seznam.cz>
2006-07-04 12:53:11 +0000

    assertion exception

---
+++ src/cz/kruch/track/AssertionFailedException.java
@@ -0,0 +1,10 @@
+// Copyright 2001-2006 Systinet Corp. All rights reserved.
+// Use is subject to license terms.
+
+package cz.kruch.track;
+
+public class AssertionFailedException extends RuntimeException {
+    public AssertionFailedException(String string) {
+        super(string);
+    }
+}

ff3faff3646a88586531120b48f11396a7424909
kruch <kruhc@seznam.cz>
2006-07-04 17:09:21 +0000

    generalized for more sentences

--- src/cz/kruch/track/util/NmeaParser.java
+++ src/cz/kruch/track/util/NmeaParser.java
@@ -3,9 +3,7 @@
 
 package cz.kruch.track.util;
 
-import api.location.QualifiedCoordinates;
 import api.location.LocationException;
-import api.location.Location;
 import cz.kruch.j2se.util.StringTokenizer;
 
 import java.util.Calendar;
@@ -15,7 +13,7 @@ import java.util.Date;
 public class NmeaParser {
     private static final Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 
-    public static Location parse(String nmea) throws LocationException {
+    public static Record parseGGA(String nmea) throws LocationException {
         Record record = new Record();
         int index = 0;
         StringTokenizer st = new StringTokenizer(nmea, ",*", true);
@@ -24,7 +22,7 @@ public class NmeaParser {
                 case 0: {
                     String token = nextToken(st, false);
                     if (!"$GPGGA".equals(token)) {
-                        throw new LocationException("Invalid NMEA record");
+                        throw new LocationException("Invalid NMEA record - GPGGA expected");
                     }
                     index++;
                 } break;
@@ -98,9 +96,77 @@ public class NmeaParser {
             }
         }
 
-        QualifiedCoordinates coordinates = new QualifiedCoordinates(record.lat, record.lon, record.altitude);
+        return record;
+    }
 
-        return new Location(coordinates, record.timestamp, record.fix, record.sat);
+    public static Record parseRMC(String nmea) throws LocationException {
+        Record record = new Record();
+        int index = 0;
+        StringTokenizer st = new StringTokenizer(nmea, ",*", true);
+        while (st.hasMoreTokens() && (index < 7)) {
+            switch (index) {
+                case 0: {
+                    String token = nextToken(st, false);
+                    if (!"$GPRMC".equals(token)) {
+                        throw new LocationException("Invalid NMEA record - GPRMC expected");
+                    }
+                    index++;
+                } break;
+                case 1: {
+                    String token = nextToken(st, false);
+                    record.timestamp = parseDate(token);
+                    index++;
+                } break;
+                case 2: {
+                    String token = nextToken(st, false);
+                    if (!token.startsWith("A")) {
+                        // invalid, bail out
+                        index = 666;
+                    }
+                    index++;
+                } break;
+                case 3: {
+                    String token = nextToken(st, false);
+                    record.lat = Double.parseDouble(token.substring(0, 2)) + Double.parseDouble(token.substring(2)) / 60D;
+                    String ns = nextToken(st, false);
+                    if (ns.startsWith("S")) {
+                        record.lat *= -1;
+                    }
+                    index++;
+                } break;
+                case 4: {
+                    String token = nextToken(st, false);
+                    record.lon = Double.parseDouble(token.substring(0, 3)) + Double.parseDouble(token.substring(3)) / 60D;
+                    String ew = nextToken(st, false);
+                    if (ew.startsWith("W")) {
+                        record.lon *= -1;
+                    }
+                    index++;
+                } break;
+                case 5: {
+                    String token = nextToken(st, false);
+                    record.speed = Float.parseFloat(token);
+                    index++;
+                } break;
+                case 6: {
+                    String token = nextToken(st, false);
+                    record.angle = Float.parseFloat(token);
+                    index++;
+                } break;
+                case 7: {
+                    // date
+                } break;
+                case 8: {
+                    // variation
+                } break;
+                case 9: {
+                    record.checkum = nextToken(st, true);
+                    index++;
+                } break;
+            }
+        }
+
+        return record;
     }
 
     private static String nextToken(StringTokenizer st, boolean last) {
@@ -116,21 +182,27 @@ public class NmeaParser {
     }
 
     private static long parseDate(String ts) {
-        int tl = Double.valueOf(ts).intValue();
+        double tl = Double.valueOf(ts).doubleValue();
         calendar.setTime(new Date(System.currentTimeMillis()));
-        int hours = tl / 10000;
+        int hours = (int) tl / 10000;
         tl -= hours * 10000;
-        int mins = tl / 100;
+        int mins = (int) tl / 100;
         tl -= mins * 100;
-        int sec = tl;
+        int sec = (int) tl;
+        int ms = (int) ((tl - sec) * 1000);
         calendar.set(Calendar.HOUR_OF_DAY, hours);
         calendar.set(Calendar.MINUTE, mins);
         calendar.set(Calendar.SECOND, sec);
+        calendar.set(Calendar.MILLISECOND, ms);
 
         return calendar.getTime().getTime();
     }
 
-    private static class Record {
+    /**
+     * Holder for both GGA and/or RMC.
+     */
+    public static class Record {
+        // GGA
         public long timestamp;
         public double lat;
         public double lon;
@@ -141,6 +213,10 @@ public class NmeaParser {
         public float geoidh;
         public int dgpst;
         public String dgpsid;
+        // RMC
+        public float speed;
+        public float angle;
+        // NMEA COMMON
         public String checkum;
     }
 }

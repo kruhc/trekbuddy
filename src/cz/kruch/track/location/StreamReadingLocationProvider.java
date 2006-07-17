cd6cb2ad6544f55ca2c8f108f7c9e35ffeff0687
kruch <kruhc@seznam.cz>
2006-07-17 12:58:34 +0000

    performance optimizations

--- src/cz/kruch/track/location/StreamReadingLocationProvider.java
+++ src/cz/kruch/track/location/StreamReadingLocationProvider.java
@@ -4,80 +4,70 @@
 package cz.kruch.track.location;
 
 import api.location.LocationProvider;
-import api.location.LocationException;
 
 import java.io.InputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.OutputStream;
 
 public abstract class StreamReadingLocationProvider extends LocationProvider {
-    protected static final String HEADER_GGA = "$GPGGA";
-    protected static final String HEADER_RMC = "$GPRMC";
+    protected static final char[] HEADER_GGA = "$GPGGA".toCharArray();
+    protected static final char[] HEADER_RMC = "$GPRMC".toCharArray();
 
-    private OutputStreamWriter observer;
-    private LocationException exception;
+    private OutputStream observer;
 
     protected StreamReadingLocationProvider(String name) {
         super(name);
     }
 
-    public synchronized LocationException getException() {
-        return exception;
-    }
-
-    protected synchronized void setException(LocationException exception) {
-        this.exception = exception;
-    }
-
-    protected synchronized void setObserver(OutputStreamWriter observer) {
+    protected void setObserver(OutputStream observer) {
         this.observer = observer;
     }
 
-    protected String nextSentence(InputStream in, String header) throws IOException {
-        String line = readLine(in);
-        while (line != null) {
-            if (header == null) {
-                break;
-            }
-            if (line.startsWith(header)) {
-                break;
-            }
-            line = readLine(in);
-        }
+    /** @deprecated change return value */
+    protected String nextSentence(InputStream in, char[] header) throws IOException {
+        char[] sb = new char[0x80];
+        int pos = 0;
 
-        return line;
-    }
-
-    private String readLine(InputStream in) throws IOException {
-        StringBuffer sb = new StringBuffer();
         boolean nl = false;
+        boolean match = false;
+        int hlen = header.length;
+
         int c = in.read();
         while (c > -1) {
-            if (sb.length() > 512) throw new IOException("Hmm, is this really NMEA tracklog?");
+            if (observer != null) {
+                observer.write(c);
+            }
+            if (c == '$') { // beginning of NMEA sentence
+                pos = 0;
+                match = true; // lie, for now :-)
+            }
+            if (match) { // header already matched or not yet enough data for header check
+                char ch = (char) c;
+                nl = (ch == '\n' || ch == '\r');
 
-            char ch = (char) c;
-            sb.append(ch);
+                if (nl) break;
 
-            nl = (ch == '\n' || ch == '\r');
-            if (nl) break;
+                sb[pos++] = ch;
+
+                if (pos >= 0x80) {
+                    throw new IOException("Hmm, is this really NMEA stream?");
+                }
+
+                if (pos == hlen) { // check header
+                    for (int i = 0; i < hlen; i++) {
+                        if (sb[i] != header[i])  {
+                            match = false;
+                            break;
+                        }
+                    }
+                }
+            }
 
             c = in.read();
         }
 
         if (nl) {
-            String line = sb.toString();
-
-            // update observer
-            if (observer != null) {
-                try {
-                    observer.write(line);
-                    observer.flush();
-                } catch (IOException e) {
-                    // ignore observer problems
-                }
-            }
-
-            return line;
+            return new String(sb, 0, pos);
         }
 
         return null;

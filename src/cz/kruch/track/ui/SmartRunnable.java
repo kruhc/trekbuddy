ca4105e4a4a04ed7c6605609432adcd67bd04ca5
kruch <kruhc@seznam.cz>
2006-10-22 07:41:50 +0000

    forgot to cimmit this

---
+++ src/cz/kruch/track/ui/SmartRunnable.java
@@ -0,0 +1,63 @@
+// Copyright 2001-2006 Systinet Corp. All rights reserved.
+// Use is subject to license terms.
+
+package cz.kruch.track.ui;
+
+import java.util.Vector;
+
+//#ifdef __LOG__
+import cz.kruch.track.util.Logger;
+//#endif
+
+public final class SmartRunnable implements Runnable {
+//#ifdef __LOG__
+    private static final Logger log = new Logger("SmartRunnable");
+//#endif
+
+    private static Vector runnables = new Vector();
+    private static SmartRunnable current = null;
+
+    private Runnable runnable;
+
+    private SmartRunnable(Runnable runnable) {
+        this.runnable = runnable;
+    }
+
+    public static synchronized void callSerially(Runnable r) {
+        runnables.addElement(new SmartRunnable(r));
+        callNext();
+    }
+
+    private static synchronized void callNext() {
+        if (current == null) {
+            if (runnables.size() > 0) {
+                current = (SmartRunnable) runnables.elementAt(0);
+//#ifdef __LOG__
+                if (log.isEnabled()) log.debug("got next runnable to call serially: " + current.runnable);
+//#endif
+                Desktop.display.callSerially(current);
+            } else {
+//#ifdef __LOG__
+                if (log.isEnabled()) log.debug("no runnable to call serially");
+//#endif
+            }
+        }
+    }
+
+    private static synchronized void removeCurrent() {
+        runnables.removeElementAt(0);
+        current = null;
+    }
+
+    public void run() {
+        try {
+            runnable.run();
+        } finally {
+//#ifdef __LOG__
+            if (log.isEnabled()) log.debug("call serially finished");
+//#endif
+            removeCurrent();
+            callNext();
+        }
+    }
+}

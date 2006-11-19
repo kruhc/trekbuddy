57193527d66ae7224f88430c944123c2e4004525
kruch <kruhc@seznam.cz>
2006-11-19 16:14:43 +0000

    sorting support

---
+++ src/cz/kruch/track/util/Arrays.java
@@ -0,0 +1,52 @@
+// Copyright 2001-2006 Systinet Corp. All rights reserved.
+// Use is subject to license terms.
+
+package cz.kruch.track.util;
+
+public class Arrays {
+
+    public static void sort(Object[] a, Comparator c) {
+        Object aux[] = new Object[a.length];
+        System.arraycopy(a, 0, aux, 0, a.length);
+        mergeSort(aux, a, 0, a.length, c);
+    }
+
+    private static void mergeSort(Object src[], Object dest[],
+                                  int low, int high, Comparator c) {
+        int length = high - low;
+
+        // Insertion sort on smallest arrays
+        if (length < 7) {
+            for (int i = low; i < high; i++)
+                for (int j = i; j > low && c.compare(dest[j - 1], dest[j]) > 0; j--)
+                    swap(dest, j, j - 1);
+            return;
+        }
+
+        // Recursively sort halves of dest into src
+        int mid = (low + high) / 2;
+        mergeSort(dest, src, low, mid, c);
+        mergeSort(dest, src, mid, high, c);
+
+        // If list is already sorted, just copy from src to dest.  This is an
+        // optimization that results in faster sorts for nearly ordered lists.
+        if (c.compare(src[mid - 1], src[mid]) <= 0) {
+            System.arraycopy(src, low, dest, low, length);
+            return;
+        }
+
+        // Merge sorted halves (now in src) into dest
+        for (int i = low, p = low, q = mid; i < high; i++) {
+            if (q >= high || p < mid && c.compare(src[p], src[q]) <= 0)
+                dest[i] = src[p++];
+            else
+                dest[i] = src[q++];
+        }
+    }
+
+    private static void swap(Object x[], int a, int b) {
+        Object t = x[a];
+        x[a] = x[b];
+        x[b] = t;
+    }
+}
---
+++ src/cz/kruch/track/util/Comparator.java
@@ -0,0 +1,8 @@
+// Copyright 2001-2006 Systinet Corp. All rights reserved.
+// Use is subject to license terms.
+
+package cz.kruch.track.util;
+
+public interface Comparator {
+    public int compare(Object o1, Object o2);
+}

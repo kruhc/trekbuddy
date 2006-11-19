c6c423ac57ecf70d479846e4f3cd3c682f539b0b
kruch <kruhc@seznam.cz>
2006-11-19 16:13:10 +0000

    more info

--- src/cz/kruch/track/ui/InfoForm.java
+++ src/cz/kruch/track/ui/InfoForm.java
@@ -5,14 +5,13 @@ package cz.kruch.track.ui;
 
 import cz.kruch.track.TrackingMIDlet;
 
-import javax.microedition.lcdui.Form;
-import javax.microedition.lcdui.StringItem;
-import javax.microedition.lcdui.Command;
-import javax.microedition.lcdui.CommandListener;
-import javax.microedition.lcdui.Displayable;
-
 import api.location.LocationException;
 
+import javax.microedition.lcdui.Form;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.StringItem;
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.Displayable;
 import java.util.TimeZone;
 
 public final class InfoForm extends Form implements CommandListener {
@@ -21,7 +20,7 @@ public final class InfoForm extends Form implements CommandListener {
         super("Info");
     }
 
-    public void show(LocationException le, Object ps) {
+    public void show(Desktop desktop, LocationException le, Object ps) {
         // gc (for memory info to be correct)
         System.gc();
 
@@ -30,6 +29,7 @@ public final class InfoForm extends Form implements CommandListener {
         append(new StringItem("Platform", TrackingMIDlet.getPlatform()));
         append(new StringItem("TimeZone", TimeZone.getDefault().getID() + "; " + TimeZone.getDefault().useDaylightTime() + "; " + TimeZone.getDefault().getRawOffset()));
         append(new StringItem("Jsr75/Fc", "resetable? " + Integer.toString(cz.kruch.track.maps.Map.fileInputStreamResetable)));
+        append(new StringItem("Desktop", "hasRepeatEvents? " + desktop.hasRepeatEvents()));
         append(new StringItem("ProviderStatus", ps == null ? "" : ps.toString()));
         append(new StringItem("ProviderError", le == null ? "" : le.toString()));
         append(new StringItem("SnapshotEncodings", System.getProperty("video.snapshot.encodings")));

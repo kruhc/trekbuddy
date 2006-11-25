130a6c70eb652b8a777d31b7a315ecf1ce3ee63e
kruch <kruhc@seznam.cz>
2006-11-25 10:06:59 +0000

    ripped out of QC

---
+++ src/api/location/MinDec.java
@@ -0,0 +1,227 @@
+// Copyright 2001-2006 Systinet Corp. All rights reserved.
+// Use is subject to license terms.
+
+package api.location;
+
+import cz.kruch.track.TrackingMIDlet;
+
+public final class MinDec {
+    private static final String[] NS = new String[]{ "N", "S" };
+    private static final String[] EW = new String[]{ "E", "W" };
+
+    private int type = QualifiedCoordinates.UNKNOWN;
+    private int sign = 0;
+    private int deg = 0;
+    private double min = 0D;
+
+    public MinDec(int type, double value) {
+        this.type = type;
+        this.sign = value < 0D ? -1 : 1;
+        value = Math.abs(value);
+        this.deg = (int) Math.floor(value);
+        value -= this.deg;
+        value *= 60D;
+        this.min = value;
+    }
+
+    private MinDec(String value) {
+        value = value.trim();
+        if (value.length() < 5) {
+            throw new IllegalArgumentException("Malformed coordinate: " + value);
+        }
+        switch (value.charAt(0)) {
+            case 'N': {
+                this.type = QualifiedCoordinates.LAT;
+                this.sign = 1;
+            } break;
+            case 'S': {
+                this.type = QualifiedCoordinates.LAT;
+                this.sign = -1;
+            } break;
+            case 'E': {
+                this.type = QualifiedCoordinates.LON;
+                this.sign = 1;
+            } break;
+            case 'W': {
+                this.type = QualifiedCoordinates.LON;
+                this.sign = -1;
+            } break;
+            default:
+                throw new IllegalArgumentException("Malformed coordinate: " + value);
+        }
+        int i = value.indexOf(TrackingMIDlet.SIGN);
+        if (i < 3) {
+            throw new IllegalArgumentException("Malformed coordinate: " + value);
+        }
+        this.deg = Integer.parseInt(value.substring(1, i).trim());
+        this.min = Double.parseDouble(value.substring(i + TrackingMIDlet.SIGN.length()).trim());
+    }
+
+    private MinDec(String value, String sign) {
+        int degl;
+        switch (sign.charAt(0)) {
+            case 'N': {
+                this.type = QualifiedCoordinates.LAT;
+                this.sign = 1;
+                degl = 2;
+            } break;
+            case 'S': {
+                this.type = QualifiedCoordinates.LAT;
+                this.sign = -1;
+                degl = 2;
+            } break;
+            case 'E': {
+                this.type = QualifiedCoordinates.LON;
+                this.sign = 1;
+                degl = 3;
+            } break;
+            case 'W': {
+                this.type = QualifiedCoordinates.LON;
+                this.sign = -1;
+                degl = 3;
+            } break;
+            default:
+                throw new IllegalArgumentException("Malformed coordinate: " + value);
+        }
+        int i = value.indexOf('.');
+        if ((type == QualifiedCoordinates.LAT && (i != 4)) || (type == QualifiedCoordinates.LON && i != 5)) {
+            throw new IllegalArgumentException("Malformed coordinate: " + value);
+        }
+        this.deg = Integer.parseInt(value.substring(0, degl));
+        this.min = Double.parseDouble(value.substring(degl));
+    }
+
+    public String getLabel() {
+        return type == QualifiedCoordinates.LAT ? "Latitude" : "Longitude";
+    }
+
+    public String[] getLetters() {
+        return type == QualifiedCoordinates.LAT ? NS : EW;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public int getSign() {
+        return sign;
+    }
+
+    public int getDeg() {
+        return deg;
+    }
+
+    public double getMin() {
+        return min;
+    }
+
+    public double doubleValue() {
+        return sign * (deg + min / 60D);
+    }
+
+    private int[] toArray() {
+        int h = deg;
+        int m = (int) Math.floor(min);
+        double l = min - m;
+        l *= 100000D;
+        int dec = (int) Math.floor(l);
+        if ((l - dec) > 0.5D) {
+            dec++;
+            if (dec == 100000) {
+                dec = 0;
+                m++;
+                if (m == 60) {
+                    m = 0;
+                    h++;
+                }
+            }
+        }
+
+        return new int[]{ h, m, dec };
+    }
+
+    public String toString() {
+        StringBuffer sb = new StringBuffer(32);
+
+        int[] hms = toArray();
+        int h = hms[0];
+        int m = hms[1];
+        int s = hms[2];
+        hms = null; // gc hint
+
+        sb.append(type == QualifiedCoordinates.LAT ? (sign == -1 ? "S" : "N") : (sign == -1 ? "W" : "E"));
+        sb.append(' ');
+        sb.append(h).append(TrackingMIDlet.SIGN).append(m).append('.');
+        if (s < 10000) {
+            sb.append('0');
+        }
+        if (s < 1000) {
+            sb.append('0');
+        }
+        if (s < 100) {
+            sb.append('0');
+        }
+        if (s < 10) {
+            sb.append('0');
+        }
+        sb.append(s);
+
+        return sb.toString();
+    }
+
+    private String toSentence() {
+        StringBuffer sb = new StringBuffer(32);
+
+        int[] hms = toArray();
+        int h = hms[0];
+        int m = hms[1];
+        int s = hms[2];
+        hms = null; // gc hint
+
+        if (type == QualifiedCoordinates.LON && h < 100) {
+            sb.append('0');
+        }
+        if (h < 10) {
+            sb.append('0');
+        }
+        sb.append(h);
+        if (m < 10) {
+            sb.append('0');
+        }
+        sb.append(m).append('.');
+        if (s < 10000) {
+            sb.append('0');
+        }
+        if (s < 1000) {
+            sb.append('0');
+        }
+        if (s < 100) {
+            sb.append('0');
+        }
+        if (s < 10) {
+            sb.append('0');
+        }
+        sb.append(s);
+        sb.append(',').append(type == QualifiedCoordinates.LAT ? (sign == -1 ? "S" : "N") : (sign == -1 ? "W" : "E"));
+
+        return sb.toString();
+    }
+
+/*
+        public static String toDecimalString(int type, double value) {
+            return (new MinDec(type, value)).toString();
+        }
+*/
+
+    public static MinDec fromDecimalString(String value) {
+        return new MinDec(value);
+    }
+
+    public static String toSentence(int type, double value) {
+        return (new MinDec(type, value)).toSentence();
+    }
+
+    public static MinDec fromSentence(String value, String sign) {
+        return new MinDec(value, sign);
+    }
+}

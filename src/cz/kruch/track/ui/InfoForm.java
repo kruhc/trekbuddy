9ecbf38d99cdfbe6a32bd6be5ddb7ddcce11961b
kruch <kruhc@seznam.cz>
2006-11-29 23:42:32 +0000

    more info

--- src/cz/kruch/track/ui/InfoForm.java
+++ src/cz/kruch/track/ui/InfoForm.java
@@ -3,8 +3,6 @@
 
 package cz.kruch.track.ui;
 
-import cz.kruch.track.TrackingMIDlet;
-
 import api.location.LocationException;
 
 import javax.microedition.lcdui.Form;
@@ -22,14 +20,16 @@ public final class InfoForm extends Form implements CommandListener {
     public void show(Desktop desktop, LocationException le, Object ps) {
         // gc (for memory info to be correct)
         System.gc();
+        long totalMemory = Runtime.getRuntime().totalMemory();
+        long freeMemory = Runtime.getRuntime().freeMemory();
 
-        append(new StringItem("Memory", Long.toString(Runtime.getRuntime().totalMemory()) + "/" + Long.toString(Runtime.getRuntime().freeMemory())));
-        append(new StringItem("AppFlags", TrackingMIDlet.getFlags()));
-        append(new StringItem("Platform", TrackingMIDlet.getPlatform()));
+        append(new StringItem("Memory", Long.toString(totalMemory) + "/" + Long.toString(freeMemory)));
+        append(new StringItem("AppFlags", cz.kruch.track.TrackingMIDlet.getFlags()));
+        append(new StringItem("Platform", cz.kruch.track.TrackingMIDlet.getPlatform()));
         append(new StringItem("TimeZone", TimeZone.getDefault().getID() + "; " + TimeZone.getDefault().useDaylightTime() + "; " + TimeZone.getDefault().getRawOffset()));
-        append(new StringItem("Jsr75/Fc", "resetable? " + Integer.toString(cz.kruch.track.maps.Map.fileInputStreamResetable)));
-        append(new StringItem("Desktop", "hasRepeatEvents? " + desktop.hasRepeatEvents()));
-        append(new StringItem("ProviderStatus", ps == null ? "" : ps.toString()));
+        append(new StringItem("Jsr75/Fc", "resetable? " + Integer.toString(cz.kruch.track.maps.Map.fileInputStreamResetable) + "; read-skip? " + com.ice.tar.TarInputStream.useReadSkip));
+        append(new StringItem("Desktop", "S60 renderer? " + cz.kruch.track.TrackingMIDlet.isNokia() + "; hasRepeatEvents? " + desktop.hasRepeatEvents()));
+        append(new StringItem("ProviderStatus", (ps == null ? "" : ps.toString()) + "; syncs=" + cz.kruch.track.location.StreamReadingLocationProvider.syncs + "; mismatches=" + cz.kruch.track.location.StreamReadingLocationProvider.mismatches));
         append(new StringItem("ProviderError", le == null ? "" : le.toString()));
         append(new StringItem("SnapshotEncodings", System.getProperty("video.snapshot.encodings")));
         addCommand(new Command("Close", Command.BACK, 1));

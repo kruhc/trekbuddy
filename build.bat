c1bd4d0463251bcfa250bd30e0b474b373729e11
kruch <kruhc@seznam.cz>
2007-09-02 08:27:31 +0000

    updated

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set JAVA_HOME=C:\Program Files\Java\jdk1.5.0_09
-set PATH=C:\Program Files\Java\jdk1.5.0_09\bin;%PATH%
+set JAVA_HOME=C:\Program Files\Java\jdk1.5.0_10
+set PATH=C:\Program Files\Java\jdk1.5.0_10\bin;%PATH%
 
 ant %1

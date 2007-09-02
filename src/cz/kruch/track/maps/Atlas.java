7026c572e6336acd986630fe09ec0f90d2012c35
kruch <kruhc@seznam.cz>
2007-09-02 08:10:57 +0000

    loercase used when searching for cal files; license preambule

--- src/cz/kruch/track/maps/Atlas.java
+++ src/cz/kruch/track/maps/Atlas.java
@@ -1,5 +1,18 @@
-// Copyright 2001-2006 Systinet Corp. All rights reserved.
-// Use is subject to license terms.
+/*
+ * Copyright 2006-2007 Ales Pour <kruhc@seznam.cz>.
+ * All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ */
 
 package cz.kruch.track.maps;
 
@@ -21,6 +34,11 @@ import api.file.File;
 import com.ice.tar.TarEntry;
 import com.ice.tar.TarInputStream;
 
+/**
+ * Atlas representation and handling.
+ *
+ * @author Ales Pour <kruhc@seznam.cz>
+ */
 public final class Atlas implements Runnable {
 
 /*
@@ -373,7 +391,7 @@ public final class Atlas implements Runnable {
                                 }
 
                                 // get ext
-                                String ext = fileEntry.substring(indexOf);
+                                String ext = fileEntry.substring(indexOf).toLowerCase();
 
                                 // calibration
                                 sb.delete(0, sb.length());

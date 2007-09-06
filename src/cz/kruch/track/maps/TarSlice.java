76a573ce460603ca81df5a79f268a3f9567c961a
kruch <kruhc@seznam.cz>
2007-09-06 19:55:48 +0000

    new class

---
+++ src/cz/kruch/track/maps/TarSlice.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2006-2007 Ales Pour <kruhc@seznam.cz>.
+ * All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ */
+
+package cz.kruch.track.maps;
+
+/**
+ * Represents map tile in tar.
+ *
+ * @author Ales Pour <kruhc@seznam.cz>
+ */
+public class TarSlice extends Slice {
+    private short blockOffset;
+
+    TarSlice(String path) throws InvalidMapException {
+        super(path);
+    }
+
+/*
+    public TarSlice(CharArrayTokenizer.Token token) throws InvalidMapException {
+        super(token);
+    }
+*/
+
+    short getBlockOffset() {
+        return blockOffset;
+    }
+
+    void setBlockOffset(short blockOffset) {
+        this.blockOffset = blockOffset;
+    }
+}

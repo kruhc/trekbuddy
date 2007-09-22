d1781fb52770760f2f846b834f696cc25b94a2de
kruch <kruhc@seznam.cz>
2007-09-22 11:26:44 +0000

    toStringBuffer moved to NavigationScreens

--- src/api/location/Location.java
+++ src/api/location/Location.java
@@ -16,32 +16,12 @@
 
 package api.location;
 
-/* bad design - dependency on external packages */
-import cz.kruch.track.configuration.Config;
-import cz.kruch.track.ui.NavigationScreens;
-import cz.kruch.track.util.SimpleCalendar;
-
-import java.util.Date;
-import java.util.Calendar;
-import java.util.TimeZone;
-
 /**
  * Represents a set of basic location information.
  * 
  * @author Ales Pour <kruhc@seznam.cz>
  */
 public final class Location {
-    private static final String STR_KN  = " kn ";
-    private static final String STR_MPH = " mph ";
-    private static final String STR_KMH = " km/h ";
-    private static final String STR_M   = " m";
-    
-/*
-    private static final Calendar CALENDAR = Calendar.getInstance(TimeZone.getDefault());
-    private static final Date DATE = new Date();
-*/
-    private static final SimpleCalendar CALENDAR = new SimpleCalendar(Calendar.getInstance(TimeZone.getDefault()));
-
     private QualifiedCoordinates coordinates;
     private long timestamp;
     private short fix;
@@ -143,65 +123,4 @@ public final class Location {
     public void setCourse(float course) {
         this.course = course;
     }
-
-    // TODO move to NavigationScreens
-    public StringBuffer toStringBuffer(StringBuffer sb) {
-/*
-        DATE.setTime(timestamp);
-        CALENDAR.setTime(DATE);
-        final int hour = CALENDAR.get(Calendar.HOUR_OF_DAY);
-        final int min = CALENDAR.get(Calendar.MINUTE);
-*/
-        CALENDAR.setTime(timestamp);
-        
-        final int hour = CALENDAR.hour;
-        final int min = CALENDAR.minute;
-
-        if (hour < 10) {
-            sb.append('0');
-        }
-        NavigationScreens.append(sb, hour).append(':');
-        if (min < 10) {
-            sb.append('0');
-        }
-        NavigationScreens.append(sb, min).append(' ');
-
-        if (fix > 0) {
-            if (Config.unitsNautical) {
-                if (speed > -1F) {
-                    NavigationScreens.append(sb, speed * 3.6F / 1.852F, 1).append(STR_KN);
-                }
-                if (course > -1F) {
-                    NavigationScreens.append(sb, (int) course).append(NavigationScreens.SIGN);
-                }
-            } else if (Config.unitsImperial) {
-                if (speed > -1F) {
-                    NavigationScreens.append(sb, speed * 3.6F / 1.609F, 1).append(STR_MPH);
-                }
-                if (coordinates.getAlt() != Float.NaN) {
-                    NavigationScreens.append(sb, coordinates.getAlt(), 1).append(STR_M);
-                }
-            } else {
-                if (speed > -1F) {
-                    NavigationScreens.append(sb, speed * 3.6F, 1).append(STR_KMH);
-                }
-                if (coordinates.getAlt() != Float.NaN) {
-                    NavigationScreens.append(sb, coordinates.getAlt(), 1).append(STR_M);
-                }
-            }
-/* rendered by OSD directly
-            if (sat > -1) {
-                sb.append(sat).append('*');
-            }
-*/
-        }
-
-        return sb;
-    }
-
-    // debug
-    public String toString() {
-        return (new Date(timestamp)).toString() + ": " + coordinates.toString() + ";" + fix + ";" + sat;
-    }
-    // ~debug
 }

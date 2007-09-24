5afdb67178c34770fdb62ec09c12fd7ac7c792a2
kruch <kruhc@seznam.cz>
2007-09-24 19:22:08 +0000

    new env

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set JAVA_HOME=C:\Program Files\Java\jdk1.5.0_10
-set PATH=C:\Program Files\Java\jdk1.5.0_10\bin;%PATH%
+set JAVA_HOME=C:\Program Files\Java\jdk1.5.0_12
+set PATH=%JAVA_HOME%\bin;%PATH%
 
 ant %1
--- buildall.bat
+++ buildall.bat
@@ -1,7 +1,7 @@
 @echo off
 
-set JAVA_HOME=C:\Program Files\Java\jdk1.5.0_10
-set PATH=C:\Program Files\Java\jdk1.5.0_10\bin;%PATH%
+set JAVA_HOME=C:\Program Files\Java\jdk1.5.0_12
+set PATH=%JAVA_HOME%\bin;%PATH%
 
 cmd /c ant generic
 cmd /c ant std

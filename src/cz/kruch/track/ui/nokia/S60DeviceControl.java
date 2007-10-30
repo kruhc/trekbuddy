9a3b126691b10c7a41845474c4bcc8510860ebcb
kruch <kruhc@seznam.cz>
2007-10-30 17:57:07 +0000

    backlight for S60

---
+++ src/cz/kruch/track/ui/nokia/S60DeviceControl.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2006-2007 Ales Pour <kruhc@seznam.cz>.
+ * All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ */
+
+package cz.kruch.track.ui.nokia;
+
+import cz.kruch.track.Resources;
+
+/**
+ * Device control implementation for SonyEricsson phones.
+ *
+ * @author Ales Pour <kruhc@seznam.cz>
+ */
+final class S60DeviceControl extends DeviceControl {
+
+    S60DeviceControl() {
+        cz.kruch.track.ui.Desktop.timer.scheduleAtFixedRate(this, 0L, 15000L);
+    }
+
+    void nextLevel() {
+        try {
+            backlight += 50;
+            if (backlight == 150) {
+                backlight = 0;
+            }
+            com.nokia.mid.ui.DeviceControl.setLights(0, backlight);
+            confirm(backlight == 0 ? Resources.getString(Resources.DESKTOP_MSG_BACKLIGHT_OFF) : Resources.getString(Resources.DESKTOP_MSG_BACKLIGHT_ON) + " (" + backlight + "%)");
+        } catch (Throwable t) {
+        }
+    }
+
+    void close() {
+        cancel();
+    }
+
+    public void run() {
+        if (backlight != 0) {
+            com.nokia.mid.ui.DeviceControl.setLights(0, backlight);
+        }
+    }
+}

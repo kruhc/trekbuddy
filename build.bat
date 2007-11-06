13585b9b29c4d94494f1522451fe7b2d39e38057
kruch <kruhc@seznam.cz>
2007-11-06 21:19:58 +0000

    all-purpose scripts

--- build.bat
+++ build.bat
@@ -5,10 +5,10 @@ set PATH=%JAVA_HOME%\bin;%PATH%
 
 if "%1"=="" goto help
 if "%2"=="" goto help
-if not exist build-%1.properties goto vendor
+if not exist properties\build-%1.properties goto vendor
 
-cmd /c ant rescompile
-cmd /c ant -Dinclude.properties=build-%1.properties %2
+cmd /c ant -f res.xml
+cmd /c ant -Dinclude.properties=properties\build-%1.properties -f build.xml %2
 goto end
 
 :vendor
--- buildall.bat
+++ buildall.bat
@@ -1,10 +1,23 @@
 @echo off
 
-set JAVA_HOME=C:\Program Files\Java\jdk1.5.0_12
-set PATH=%JAVA_HOME%\bin;%PATH%
+if "%1"=="" goto help
+if not exist properties\build-%1.properties goto vendor
 
-cmd /c ant generic %*
-cmd /c ant std %*
-cmd /c ant j9 %*
-cmd /c ant rim41 %*
-cmd /c ant rim42 %*
+cmd /c build %1 generic
+cmd /c build %1 std
+cmd /c build %1 j9
+cmd /c build %1 rim41
+cmd /c build %1 rim42
+goto end
+
+:vendor
+echo No such vendor - %1
+goto end
+
+:help
+echo Usage: %0 vendor
+echo Example:
+echo        %0 public
+goto end
+
+:end

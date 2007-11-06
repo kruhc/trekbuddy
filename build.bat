21ffa1d34e41dfc9e3f3ce13a66544b00bedd2bd
kruch <kruhc@seznam.cz>
2007-11-06 12:11:04 +0000

    more generic

--- build.bat
+++ build.bat
@@ -3,4 +3,22 @@
 set JAVA_HOME=C:\Program Files\Java\jdk1.5.0_12
 set PATH=%JAVA_HOME%\bin;%PATH%
 
-ant -Dinclude.properties=build.properties %*
+if "%1"=="" goto help
+if "%2"=="" goto help
+if not exist build-%1.properties goto vendor
+
+cmd /c ant rescompile
+cmd /c ant -Dinclude.properties=build-%1.properties %2
+goto end
+
+:vendor
+echo No such vendor - %1
+goto end
+
+:help
+echo Usage: %0 vendor device
+echo Example:
+echo        %0 public generic
+goto end
+
+:end

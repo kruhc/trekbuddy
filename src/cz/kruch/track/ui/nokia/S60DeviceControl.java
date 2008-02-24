7707ed7ab9137e8819196b560a16efc95235fafe
kruch <kruhc@seznam.cz>
2008-02-24 11:34:40 +0000

    finer control

--- src/cz/kruch/track/ui/nokia/S60DeviceControl.java
+++ src/cz/kruch/track/ui/nokia/S60DeviceControl.java
@@ -31,8 +31,8 @@ final class S60DeviceControl extends DeviceControl {
 
     void nextLevel() {
         try {
-            backlight += 50;
-            if (backlight == 150) {
+            backlight += 25;
+            if (backlight > 100) {
                 backlight = 0;
             }
             com.nokia.mid.ui.DeviceControl.setLights(0, backlight);

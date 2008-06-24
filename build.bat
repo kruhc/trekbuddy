9f8ec52addbf2142fd2a0a5ff6040ef32779c5f5
kruch <kruhc@seznam.cz>
2008-06-24 20:44:34 +0000

    improved build process; new apis

--- build.bat
+++ build.bat
@@ -5,10 +5,11 @@ set PATH=%JAVA_HOME%\bin;%PATH%
 
 if "%1"=="" goto help
 if "%2"=="" goto help
+if "%3"=="" goto help
 if not exist properties\build-%1.properties goto vendor
 
-cmd /c ant -l logs\%1.%2-res.log -f res.xml %1
-cmd /c ant -Dinclude.properties=properties\build-%1.properties -l logs\%1.%2.log -f build.xml %2
+cmd /c ant -l dist\%1\%2\build-res.log -f res.xml -Dlocale=%3
+cmd /c ant -Dinclude.properties=properties\build-%1.properties -l dist\%1\%2\build.log -f build.xml %2
 goto end
 
 :vendor
@@ -16,9 +17,9 @@ echo No such vendor - %1
 goto end
 
 :help
-echo Usage: %0 vendor device
+echo Usage: %0 vendor device locale
 echo Example:
-echo        %0 public generic
+echo        %0 public generic en_US
 goto end
 
 :end
--- buildall.bat
+++ buildall.bat
@@ -3,11 +3,11 @@
 if "%1"=="" goto help
 if not exist properties\build-%1.properties goto vendor
 
-cmd /c build %1 generic
-cmd /c build %1 std
-cmd /c build %1 j9
-cmd /c build %1 rim41
-cmd /c build %1 rim42
+cmd /c build %1 generic en_US
+cmd /c build %1 std en_US
+cmd /c build %1 j9 en_US
+cmd /c build %1 rim41 en_US
+cmd /c build %1 rim42 en_US
 goto end
 
 :vendor

79f874b99e682c573e42cc678cc1f211927f6201
kruch <kruhc@seznam.cz>
2008-06-30 19:31:25 +0000

    more flexible builds

--- build.bat
+++ build.bat
@@ -8,8 +8,7 @@ if "%2"=="" goto help
 if "%3"=="" goto help
 if not exist properties\build-%1.properties goto vendor
 
-cmd /c ant -l dist\%1\%2\build-res.log -f res.xml -Dlocale=%3
-cmd /c ant -Dinclude.properties=properties\build-%1.properties -l dist\%1\%2\build.log -f build.xml %2
+cmd /c ant -Dinclude.properties=properties\build-%1.properties -Dlocale=%3 -l dist\%1\%2\build.log -f build.xml %2
 goto end
 
 :vendor

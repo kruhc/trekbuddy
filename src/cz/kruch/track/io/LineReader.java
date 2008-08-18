94c4fbd1715d843ce67eaf216c473182500b88ea
kruch <kruhc@seznam.cz>
2008-08-18 19:50:00 +0000

    infinite stream commented out

--- src/cz/kruch/track/io/LineReader.java
+++ src/cz/kruch/track/io/LineReader.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2006-2007 Ales Pour <kruhc@seznam.cz>.
- * All Rights Reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- */
+// @LICENSE@
 
 package cz.kruch.track.io;
 
@@ -27,25 +13,27 @@ import java.io.InputStreamReader;
  *
  * @author Ales Pour <kruhc@seznam.cz>
  */
-public final class LineReader {
+public final class LineReader extends InputStreamReader {
 //#ifdef __LOG__
     private static final cz.kruch.track.util.Logger log = new cz.kruch.track.util.Logger("LineReader");
 //#endif
 
     public static final String EMPTY_LINE = "";
 
+    private static final int MAX_LINE_LENGTH    = 256;
     private static final int DEFAULT_BUFF_SIZE  = 1024;
-    private static final int MAX_LINE           = 256;
 
+/*
+    private static InfiniteInputStream iin;
+    private static InputStreamReader iir;
+*/
+
+    private CharArrayTokenizer.Token token;
     private char[] buffer;
     private int count;
     private int position;
 
-    private CharArrayTokenizer.Token token;
-
-    private static InfiniteInputStream iin;
-    private static InputStreamReader iir;
-
+/*
     private static void ensureReader(InputStream in) {
 //#ifdef __LOG__
         if (log.isEnabled()) log.debug("get reader for " + in);
@@ -58,25 +46,37 @@ public final class LineReader {
         }
         if (iir == null) {
             iir = new InputStreamReader(iin);
+        } else {
+            try {
+                iir.reset();
+            } catch (IOException e) {
+                // ignore - reader does not support reset
+            }
         }
+    }
+*/
+
+    public LineReader(InputStream in, int buffSize) {
+/*
+        ensureReader(in);
+*/
+        super(in);
+        this.token = new CharArrayTokenizer.Token();
+        this.buffer = new char[buffSize];
+        this.token.array = buffer;
     }
 
     public LineReader(InputStream in) {
         this(in, DEFAULT_BUFF_SIZE);
     }
 
-    public LineReader(InputStream in, int buffSize) {
-        ensureReader(in);
-        this.buffer = new char[buffSize];
-        this.token = new CharArrayTokenizer.Token();
-        this.token.array = buffer;
-    }
-
+/*
     public void close() throws IOException {
         iin.close();
     }
+*/
 
-    public String readLine(final boolean ignoreLF) throws IOException {
+    public String readLine(boolean ignoreLF) throws IOException {
         final CharArrayTokenizer.Token result = readToken(ignoreLF);
         if (result != null) {
             if (!result.isEmpty()) {
@@ -88,20 +88,15 @@ public final class LineReader {
     }
 
     public CharArrayTokenizer.Token readToken(final boolean ignoreLF) throws IOException {
-/* always true, see contructor
-        if (token == null) {
-            throw new IllegalStateException("Not in token mode");
-        }
-*/
         if (count == -1) {
             return null;
         }
 
         final char[] buffer = this.buffer;
-        final int BUFF_SIZE = buffer.length;
-        final CharArrayTokenizer.Token token = this.token;
+        final int buffSize = buffer.length;
 
-        if (position > BUFF_SIZE - MAX_LINE) { // reaching end of buffer
+        /* reaching end of buffer - shift remaining bytes and refill */
+        if (position > buffSize - MAX_LINE_LENGTH) {
             System.arraycopy(buffer, position, buffer, 0, count - position);
             count -= position;
             position = 0;
@@ -111,12 +106,12 @@ public final class LineReader {
         int offset = this.position;
         int chars = 0;
 
-        while (offset < BUFF_SIZE) {
+        while (offset < buffSize) {
             final int c;
             if (offset < count) {
                 c = buffer[offset++];
             } else {
-                final int _count = iir.read(buffer, offset, BUFF_SIZE - offset);
+                final int _count = read(buffer, offset, buffSize - offset);
                 if (_count == -1) {
                     this.count = -1;
                     if (chars == 0) {
@@ -131,27 +126,88 @@ public final class LineReader {
                 }
             }
 
-            if (c == '\r') {
-                // '\n' should follow
-            } else if (c == '\n') {
-                break;
-            } else {
+            if (c >= 0x20) { // most common situation
                 chars++;
+            } else if (c == '\r') { // '\n' should follow
+            } else if (c == '\n') { // end of line
+                break;
             }
+            /* TODO other control chars ignored? */
         }
 
-        if (offset < BUFF_SIZE) {
+        if (offset < buffSize) {
+
+            // local ref
+            final CharArrayTokenizer.Token token = this.token;
 
             // prepare token
             token.begin = position;
             token.length = chars;
 
-            // store input buffer position
+            // update in-buffer position
             this.position = offset;
 
             return token;
         }
 
-        throw new IllegalStateException("Line length > " + MAX_LINE);
+        throw new IllegalStateException("Line length > " + MAX_LINE_LENGTH);
     }
+
+/*
+    static final class InfiniteInputStream extends InputStream {
+
+        private InputStream in;
+
+        public InfiniteInputStream(InputStream in) {
+            this.in = in;
+        }
+
+        public void use(InputStream in) throws IllegalStateException {
+            if (this.in != null) {
+                throw new IllegalStateException("Stream already in use");
+            }
+            this.in = in;
+        }
+
+        public int read() throws IOException {
+            return in.read();
+        }
+
+        public int read(byte[] b) throws IOException {
+            return in.read(b);
+        }
+
+        public int read(byte[] b, int off, int len) throws IOException {
+            return in.read(b, off, len);
+        }
+
+        public long skip(long n) throws IOException {
+            return in.skip(n);
+        }
+
+        public int available() throws IOException {
+            return in.available();
+        }
+
+        public synchronized void mark(int readlimit) {
+            in.mark(readlimit);
+        }
+
+        public synchronized void reset() throws IOException {
+            if (in == null) { // situation: called by reader when getting prepared for reuse
+                return;
+            }
+            in.reset();
+        }
+
+        public boolean markSupported() {
+            return in.markSupported();
+        }
+
+        public void close() throws IOException {
+            in.close();
+            in = null; // gc hint
+        }
+    }
+*/
 }

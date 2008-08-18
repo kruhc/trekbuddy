d90d342d526cf71c566f4dc32bf4798501f4566b
kruch <kruhc@seznam.cz>
2008-08-18 19:31:23 +0000

    refactored for better perf with slow streams

--- src/cz/kruch/j2se/io/BufferedInputStream.java
+++ src/cz/kruch/j2se/io/BufferedInputStream.java
@@ -1,9 +1,4 @@
-/*
- * @(#)BufferedInputStream.java	1.43 03/01/23
- *
- * Copyright 2003 Sun Microsystems, Inc. All rights reserved.
- * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
+// @LICENSE@
 
 package cz.kruch.j2se.io;
 
@@ -21,7 +16,7 @@ public final class BufferedInputStream extends InputStream {
     /** number of bytes in the buffer */
     private int count;
     /** current position in the buffer */
-    private int pos;
+    private int position;
 
     /**
      * Constructor.
@@ -30,9 +25,6 @@ public final class BufferedInputStream extends InputStream {
      * @param size buffer size
      */
     public BufferedInputStream(InputStream in, int size) {
-        if (size <= 0) {
-            throw new IllegalArgumentException("Buffer size <= 0");
-        }
         this.in = in;
         this.buffer = new byte[size];
     }
@@ -47,7 +39,7 @@ public final class BufferedInputStream extends InputStream {
     public InputStream setInputStream(InputStream in) {
         this.in = null; // gc hint
         this.in = in;
-        this.pos = this.count = 0;
+        this.position = this.count = 0;
 
         return this;
     }
@@ -57,14 +49,33 @@ public final class BufferedInputStream extends InputStream {
      */
 
     public int read() throws IOException {
+/* // original variant
         if (pos >= count) {
-            fill();
+            fillBuffer();
             if (pos >= count) {
                 return -1;
             }
         }
 
         return buffer[pos++] & 0xff;
+*/
+        /* Optimistic variant first, at the expense of duplicate code... */
+        if (position < count) {
+            return buffer[position++] & 0xff;
+        }
+
+        byte[] buffer = this.buffer;
+        
+        /* buffer is depleted, fill it */
+        position = 0;
+        count = in.read(buffer, 0, buffer.length);
+
+        /* got something */
+        if (position < count) {
+            return buffer[position++] & 0xff;
+        }
+
+        return -1;
     }
 
     public int read(byte[] b) throws IOException {
@@ -72,90 +83,52 @@ public final class BufferedInputStream extends InputStream {
     }
 
     public int read(byte[] b, int off, int len) throws IOException {
-        if ((off | len | (off + len) | (b.length - (off + len))) < 0) {
-            throw new IndexOutOfBoundsException();
-        } else if (len == 0) {
-            return 0;
-        }
+        int n = 0;
 
-        int n = read1(b, off, len);
-        if (n <= 0) {
-            return n;
-        }
-
-        while ((n < len) && (in.available() > 0)) {
-            int n1 = read1(b, off + n, len - n);
-            if (n1 <= 0) {
-                break;
+        if (len > 0) {
+            final int avail = count - position;
+            if (avail > 0) {
+                n = avail < len ? avail : len;
+                System.arraycopy(buffer, position, b, off, n);
+                position += n;
+            } else {
+                n = in.read(b, off, len);
             }
-            n += n1;
         }
 
         return n;
     }
 
     public long skip(long n) throws IOException {
-        if (n <= 0) {
-            return 0;
+        if (n > 0) {
+            final long avail = count - position;
+            if (avail > 0) {
+                n = avail < n ? avail : n;
+                position += n;
+            } else { 
+                n = in.skip(n);
+            }
         }
 
-        final long avail = count - pos;
-
-        if (avail <= 0) {
-            return in.skip(n);
-        }
-
-        final long skipped = avail < n ? avail : n;
-
-        pos += skipped;
-
-        return skipped;
+        return n;
     }
 
     public int available() throws IOException {
-        return (count - pos) + in.available();
+        final int avail = count - position;
+        if (avail > 0) {
+            return avail;
+        }
+        return in.available();
     }
 
     public void close() throws IOException {
+        position = count = 0; // prepare for reuse
         if (in != null) { // beware it may be null
-            in.close();
-            in = null; // gc hint
-        }
-        pos = count = 0; // prepare for reuse
-    }
-
-    /*
-     * private stuff
-     */
-
-    private void fill() throws IOException {
-        count = pos = 0;
-        int n = in.read(buffer, pos, buffer.length - pos);
-        if (n > 0) {
-            count = n + pos;
-        }
-    }
-
-    private int read1(byte[] b, int off, int len) throws IOException {
-        int avail = count - pos;
-        if (avail <= 0) {
-            /* If the requested length is at least as large as the buffer, and
-              if there is no mark/reset activity, do not bother to copy the
-              bytes into the local buffer.  In this way buffered streams will
-              cascade harmlessly. */
-            if (len >= buffer.length) {
-                return in.read(b, off, len);
-            }
-            fill();
-            avail = count - pos;
-            if (avail <= 0) {
-                return -1;
+            try {
+                in.close();
+            } finally {
+                in = null; // gc hint
             }
         }
-        final int n = (avail < len) ? avail : len;
-        System.arraycopy(buffer, pos, b, off, n);
-        pos += n;
-
-        return n;
     }
 }

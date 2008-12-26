0fb77a4f49c7f9c36805b7c2c8bd1d0a8ab8b1a3
kruch <kruhc@seznam.cz>
2008-12-26 12:17:19 +0000

    using dummy image - scattered maps support

--- src/cz/kruch/track/maps/Map.java
+++ src/cz/kruch/track/maps/Map.java
@@ -1,25 +1,11 @@
-/*
- * Copyright 2006-2007 Ales Pour <kruhc@seznam.cz>.
- * All Rights Reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- */
+// @LICENSE@
 
 package cz.kruch.track.maps;
 
 import java.io.IOException;
 import java.util.Vector;
 
-import cz.kruch.j2se.io.BufferedInputStream;
+import api.io.BufferedInputStream;
 
 import cz.kruch.track.configuration.Config;
 import cz.kruch.track.maps.io.LoaderIO;
@@ -651,6 +637,7 @@ public final class Map implements Runnable {
                             try {
                                 loadSlice(slice);
                             } catch (Throwable t) {
+                                slice.setImage(Slice.NO_IMAGE);
                                 throw new InvalidMapException(Resources.getString(Resources.DESKTOP_MSG_SLICE_LOAD_FAILED) + ": " + t.toString());
                             }
 

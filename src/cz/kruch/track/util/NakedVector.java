4f2a17d1767135f93c2c73915bbcf2da9053c90d
kruch <kruhc@seznam.cz>
2008-12-26 12:05:51 +0000

    license; clone constructor

--- src/cz/kruch/track/util/NakedVector.java
+++ src/cz/kruch/track/util/NakedVector.java
@@ -1,36 +1,22 @@
-/*
- * Copyright 2006-2007 Ales Pour <kruhc@seznam.cz>.
- * All Rights Reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- */
+// @LICENSE@
 
 package cz.kruch.track.util;
 
 import java.util.Vector;
 
-public final class NakedVector extends Vector {
+public class NakedVector extends Vector {
 
     public NakedVector(int initialCapacity, int capacityIncrement) {
         super(initialCapacity, capacityIncrement);
     }
 
-    public Object[] getData() {
-        return super.elementData;
+    public NakedVector(NakedVector master) {
+        super(master.size(), 0);
+        System.arraycopy(master.elementData, 0, super.elementData, 0, master.elementCount);
+        super.elementCount = master.elementCount;
     }
 
-    public Object[] stealData() {
-        Object[] result = super.elementData;
-        super.elementData = null;
-        return result;
+    public Object[] getData() {
+        return super.elementData;
     }
 }

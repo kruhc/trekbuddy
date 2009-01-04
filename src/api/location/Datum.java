a2ee00f8d5498fff900be29984b8f4cbfa0b6eda
kruch <kruhc@seznam.cz>
2009-01-04 16:02:20 +0000

    static member injected

--- src/api/location/Datum.java
+++ src/api/location/Datum.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2006-2007 Ales Pour <kruhc@seznam.cz>.
- * All Rights Reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- */
+// @LICENSE@
 
 package api.location;
 
@@ -22,8 +8,7 @@ package api.location;
  * @author Ales Pour <kruhc@seznam.cz>
  */
 public final class Datum {
-    public static final Datum WGS_84 = new Datum("WGS 84", Ellipsoid.ELLIPSOIDS[Ellipsoid.ELLIPSOIDS.length - 1], 0, 0, 0);
-
+    public static Datum WGS_84;
     public static Datum contextDatum;
 
     public final String name;

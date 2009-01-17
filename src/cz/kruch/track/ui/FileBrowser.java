4f2343e0318d2642666e05906911574af4e043eb
kruch <kruhc@seznam.cz>
2009-01-17 21:23:48 +0000

    removed debugging code

--- src/cz/kruch/track/ui/FileBrowser.java
+++ src/cz/kruch/track/ui/FileBrowser.java
@@ -1,33 +1,19 @@
-/*
- * Copyright 2006-2007 Ales Pour <kruhc@seznam.cz>.
- * All Rights Reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- */
+// @LICENSE@
 
 package cz.kruch.track.ui;
 
+import cz.kruch.track.configuration.Config;
 import cz.kruch.track.event.Callback;
 import cz.kruch.track.Resources;
-import cz.kruch.track.configuration.Config;
 
 import javax.microedition.lcdui.List;
 import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Displayable;
 import javax.microedition.lcdui.Ticker;
+import java.io.IOException;
 import java.util.Enumeration;
 import java.util.Vector;
-import java.io.IOException;
 
 import api.file.File;
 import api.util.Comparator;
@@ -178,8 +164,8 @@ public final class FileBrowser implements CommandListener, Runnable, Comparator 
 //#endif
 
                 // list dir content
-                if (isDir) { try {
-                    show(file); } catch (Exception e) { throw new IllegalStateException("3. [" + file.getURL() + "];" + e.toString()); }
+                if (isDir) {
+                    show(file);
                 } else { // otherwise we got a file selected
                     quit(null);
                 }

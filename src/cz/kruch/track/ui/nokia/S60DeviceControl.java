47ab786337efc991da1e2b145b4b5399217936cb
kruch <kruhc@seznam.cz>
2009-07-24 07:32:41 +0000

    S60 3rd FP2 handling

--- src/cz/kruch/track/ui/nokia/S60DeviceControl.java
+++ src/cz/kruch/track/ui/nokia/S60DeviceControl.java
@@ -3,11 +3,12 @@
 package cz.kruch.track.ui.nokia;
 
 import cz.kruch.track.device.SymbianService;
+import cz.kruch.track.ui.Desktop;
 
 import java.io.IOException;
 
 /**
- * Device control implementation for S60 phones.
+ * Device control implementation for S60/UIQ phones.
  *
  * @author Ales Pour <kruhc@seznam.cz>
  */
@@ -17,25 +18,62 @@ final class S60DeviceControl extends NokiaDeviceControl {
 
     S60DeviceControl() throws IOException {
         if (cz.kruch.track.TrackingMIDlet.uiq) {
-            this.name = "Symbian/UIQ";
+            this.name = "UIQ";
         } else {
-            this.name = "Symbian/S60";
+            if (cz.kruch.track.TrackingMIDlet.s60rdfp2) {
+                this.name = "S60 (3rd FP2)";
+                this.values[0] = 10; // N5800 workaround???
+            } else {
+                this.name = "S60";
+                try {
+                    this.inactivity = SymbianService.openInactivity();
+                } catch (Exception e) { // IOE or SE
+                    // service not running/accessible
+                }
+            }
             this.cellIdProperty = "com.nokia.mid.cellid";
         }
-        this.inactivity = SymbianService.openInactivity();
     }
 
-    /** @overriden */
+
+    /** @Override */
     protected void setLights() {
-        inactivity.setLights(values[backlight]);
+
+        // old S60 or UIQ and service avail
+        if (inactivity != null) {
+            inactivity.setLights(values[backlight]);
+        }
+
+        // S60, not UIQ
         if (cz.kruch.track.TrackingMIDlet.nokia) {
+
+            // set light level via Nokia UI API
             super.setLights();
+
+            // service either not avail or not needed (S60 3rd FP2+)
+            if (inactivity == null) {
+                if (backlight == STATUS_OFF) {
+                    if (task != null) {
+                        task.cancel();
+                        task = null;
+                    }
+                } else if (task == null) {
+                    Desktop.timer.scheduleAtFixedRate(task = new DeviceControl(), 5000L, 5000L);
+                }
+            }
         }
     }
 
-    /** @overriden */
+    /** @Override */
     void close() {
-        inactivity.close();
+        if (inactivity != null) {
+            inactivity.close();
+        }
         super.close();
+    }
+
+    /** @Override */
+    public void run() {
+        super.setLights();
     }
 }

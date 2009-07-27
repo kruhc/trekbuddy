e6fb1835f0c3408e1f7a4d9714bc71273b3f73fb
kruch <kruhc@seznam.cz>
2009-07-27 19:54:03 +0000

    android mmapi

---
+++ src/cz/kruch/track/fun/AndroidCamera.java
@@ -0,0 +1,51 @@
+// @LICENSE@
+
+package cz.kruch.track.fun;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+
+import javax.microedition.media.MediaException;
+import javax.microedition.lcdui.Form;
+import java.util.Vector;
+
+/**
+ * Android multimedia.
+ */
+final class AndroidCamera extends Camera {
+//#ifdef __LOG__
+	private static final cz.kruch.track.util.Logger log = new cz.kruch.track.util.Logger("AndroidCamera");
+//#endif
+
+	public AndroidCamera() {
+	}
+
+	void getResolutions(final Vector v) {
+	}
+
+	void beforeShoot() throws MediaException {
+	}
+
+	void createFinder(final Form form) throws MediaException {
+	}
+
+	boolean playSound(final String url) {
+    	MediaPlayer player = MediaPlayer.create(org.microemu.android.MicroEmulator.context,
+                                                Uri.parse(url));
+		try {
+			player.start();
+			return true;
+		} catch (Exception e) { // NPE od IOE
+//#ifdef __LOG__
+			e.printStackTrace();
+//#endif
+			if (player != null) {
+				player.release();
+			}
+		}
+		return false;
+	}
+
+	public void run() {
+	}
+}

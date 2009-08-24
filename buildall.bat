163d6aae97886c2ddcdf35deee67e824a49a47bd
kruch <kruhc@seznam.cz>
2009-08-24 12:33:15 +0000

    new target

--- buildall.bat
+++ buildall.bat
@@ -3,11 +3,18 @@
 if "%1"=="" goto help
 if not exist properties\build-%1.properties goto vendor
 
+echo Build version 'standard' ...
 cmd /c build %1 standard en_US
-cmd /c build %1 minimal en_US
+echo Build version 'standard lite' ...
+cmd /c build %1 standard-lite en_US
+echo Build version 'basic' ...
+cmd /c build %1 basic en_US
+echo Build version 'j9' ...
 cmd /c build %1 j9 en_US
-cmd /c build %1 rim41 en_US
+rem cmd /c build %1 rim41 en_US
+echo Build version 'rim42' ...
 cmd /c build %1 rim42 en_US
+echo Build version 'android' ...
 cmd /c build %1 android en_US
 goto end
 

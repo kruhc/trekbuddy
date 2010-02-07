ee82d898da40a614327af495f68546f72d299dc1
kruch <kruhc@seznam.cz>
2010-02-07 10:21:06 +0000

    sensor support

--- src/cz/kruch/track/ui/View.java
+++ src/cz/kruch/track/ui/View.java
@@ -80,29 +80,10 @@ abstract class View {
     public void sizeChanged(int w, int h) {
     }
 
+    public int orientationChanged(int heading) {
+        return isVisible ? Desktop.MASK_SCREEN : Desktop.MASK_NONE;
+    }
+    
     public abstract void render(Graphics g, Font f, int mask);
     public abstract int locationUpdated(Location l);
-
-    /*
-     * Hack for rendering.
-     */
-/*
-    / ** @deprecated temporary solution until renderer is improved * /
-    private GameCanvas gameCanvas;
-
-    / ** @deprecated temporary solution until renderer is improved * /
-    final void setCanvas(GameCanvas gameCanvas) {
-        this.gameCanvas = gameCanvas;
-    }
-
-    / ** @deprecated temporary solution until renderer is improved * /
-    protected final void flushGraphics() {
-        gameCanvas.flushGraphics();
-    }
-
-    / ** @deprecated temporary solution until renderer is improved * /
-    protected final void flushGraphics(final int[] clip) {
-        gameCanvas.flushGraphics(clip[0], clip[1], clip[2], clip[3]);
-    }
-*/
 }

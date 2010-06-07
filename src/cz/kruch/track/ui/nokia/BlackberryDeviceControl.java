5424aca22fe5de8504ea0fd806cf05b26d7cba25
kruch <kruhc@seznam.cz>
2010-06-07 15:32:12 +0000

    load/save

--- src/cz/kruch/track/ui/nokia/BlackberryDeviceControl.java
+++ src/cz/kruch/track/ui/nokia/BlackberryDeviceControl.java
@@ -8,11 +8,18 @@ package cz.kruch.track.ui.nokia;
  * @author Ales Pour <kruhc@seznam.cz>
  */
 final class BlackberryDeviceControl extends DeviceControl {
+    private static final long KEY = 0xa224326efdc3cb05L; // long hash of "net.trekbuddy.midlet"
+
+    static net.rim.device.api.system.PersistentObject store;
+    static {
+        store = net.rim.device.api.system.PersistentStore.getPersistentObject(KEY);
+    }
 
     BlackberryDeviceControl() {
         this.name = "Blackberry";
     }
 
+    /** @overriden */
     boolean isSchedulable() {
         return true;
     }
@@ -50,6 +57,24 @@ final class BlackberryDeviceControl extends DeviceControl {
         if (backlight != STATUS_OFF) {
             net.rim.device.api.system.Backlight.enable(false);
             net.rim.device.api.system.Backlight.enable(true);
+        }
+    }
+
+    /** @Override */
+    void loadDatadir() {
+        synchronized (store) {
+            final String value = (String) store.getContents();
+            if (value != null && value.length() != 0) {
+                cz.kruch.track.configuration.Config.dataDir = value;
+            }
+        }
+    }
+
+    /** @Override */
+    void saveDatadir() {
+        synchronized (store) {
+            store.setContents(cz.kruch.track.configuration.Config.dataDir);
+            store.commit();
         }
     }
 }

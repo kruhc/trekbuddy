1a97e2abb90c12cf03fd722606c26b2a4e93f3e0
kruch <kruhc@seznam.cz>
2010-11-28 17:01:54 +0000

    catchnging Throwable

--- src/cz/kruch/track/ui/nokia/SonyEricssonDeviceControl.java
+++ src/cz/kruch/track/ui/nokia/SonyEricssonDeviceControl.java
@@ -18,7 +18,7 @@ final class SonyEricssonDeviceControl extends DeviceControl {
             Class.forName("java.rmi.Remote");
             this.name = "SonyEricsson/JP6+";
             this.jp6plus = true;
-        } catch (ClassNotFoundException e) {
+        } catch (Throwable t) {
             this.name = "SonyEricsson";
         }
     }

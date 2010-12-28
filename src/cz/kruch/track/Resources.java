56f2b1456f9b21727678515bcd2f9e92ae08c445
kruch <kruhc@seznam.cz>
2010-12-28 23:40:08 +0000

    new entry

--- src/cz/kruch/track/Resources.java
+++ src/cz/kruch/track/Resources.java
@@ -363,6 +363,7 @@ public final class Resources {
     public static final short CFG_TWEAKS_FLD_LOWMEM_IO          = 3811;
     public static final short CFG_TWEAKS_FLD_NUMERIC_INPUT_HACK = 3812;
     public static final short CFG_TWEAKS_FLD_DRAW_MAIN_MENU     = 3813;
+    public static final short CFG_TWEAKS_FLD_EXTERNAL_CFG_BACKUP = 3814;
     /* info - commands */
     public static final short INFO_CMD_DETAILS                  = 4000;
     /* info - items */

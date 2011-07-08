b3ae73d810ecf57a0a2b17b85624d68738ad52cf
kruch <kruhc@seznam.cz>
2011-07-08 19:41:02 +0000

    added project() method; fixed float and double literals

--- src/api/location/QualifiedCoordinates.java
+++ src/api/location/QualifiedCoordinates.java
@@ -6,6 +6,8 @@ import cz.kruch.track.util.ExtraMath;
 
 public final class QualifiedCoordinates implements GeodeticPosition {
 
+    public static QualifiedCoordinates INVALID = newInstance(Double.NaN, Double.NaN);
+    
     public static final int UNKNOWN = 0;
     public static final int LAT = 1;
     public static final int LON = 2;
@@ -54,7 +56,7 @@ public final class QualifiedCoordinates implements GeodeticPosition {
     }
 
     public static synchronized void releaseInstance(final QualifiedCoordinates qc) {
-        if (qc != null) {
+        if (qc != null && qc != INVALID) {
             if (countFree < pool.length) {
                 pool[countFree++] = qc;
             }
@@ -112,15 +114,50 @@ public final class QualifiedCoordinates implements GeodeticPosition {
         this.vAccuracy = accuracy;
     }
 
-    public float distance(QualifiedCoordinates neighbour) {
-        return distance(lat, lon, neighbour.lat, neighbour.lon);
+    public float azimuthTo(final QualifiedCoordinates to) {
+        double result = 0D;
+
+        final int ilat1 = (int)(0.50D + lat * 360000D);
+        final int ilat2 = (int)(0.50D + to.lat * 360000D);
+        final int ilon1 = (int)(0.50D + lon * 360000D);
+        final int ilon2 = (int)(0.50D + to.lon * 360000D);
+
+        final double lat1 = Math.toRadians(lat);
+        final double lon1 = Math.toRadians(lon);
+        final double lat2 = Math.toRadians(to.lat);
+        final double lon2 = Math.toRadians(to.lon);
+
+        if (ilat1 == ilat2 && ilon1 == ilon2) {
+            return (float) result;
+        } else if (ilon1 == ilon2) {
+            if (ilat1 > ilat2) {
+                result = 180D;
+            }
+        } else {
+            final double c = ExtraMath.acos(Math.sin(lat2) * Math.sin(lat1) + Math.cos(lat2) * Math.cos(lat1) * Math.cos((lon2 - lon1)));
+            final double A = ExtraMath.asin(Math.cos(lat2) * Math.sin((lon2 - lon1)) / Math.sin(c));
+            result = Math.toDegrees(A);
+
+            if ((ilat2 > ilat1) && (ilon2 > ilon1)) {
+            } else if ((ilat2 < ilat1) && (ilon2 < ilon1)){
+                result = 180D - result;
+            } else if ((ilat2 < ilat1) && (ilon2 > ilon1)) {
+                result = 180D - result;
+            } else if ((ilat2 > ilat1) && (ilon2 < ilon1)) {
+                result += 360D;
+            }
+        }
+
+        return (float) result;
     }
 
-    public static float distance(final double lat, 
-                                 final double lon,
-                                 final double neighbourLat,
-                                 final double neighbourLon) {
-        if (lat == neighbourLat && lon == neighbourLon) {
+    public float distance(final QualifiedCoordinates to) {
+        return distance(lat, lon, to.lat, to.lon);
+    }
+
+    public static float distance(final double lat, final double lon,
+                                 final double toLat, final double toLon) {
+        if (lat == toLat && lon == toLon) {
             return 0F;
         }
 
@@ -129,12 +166,12 @@ public final class QualifiedCoordinates implements GeodeticPosition {
          */
         final double lat1 = Math.toRadians(lat);
         final double lon1 = - Math.toRadians(lon);
-        final double lat2 = Math.toRadians(neighbourLat);
-        final double lon2 = - Math.toRadians(neighbourLon);
+        final double lat2 = Math.toRadians(toLat);
+        final double lon2 = - Math.toRadians(toLon);
 
-        final double F = (lat1 + lat2) / 2.0D;
-        final double G = (lat1 - lat2) / 2.0D;
-        final double L = (lon1 - lon2) / 2.0D;
+        final double F = (lat1 + lat2) / 2;
+        final double G = (lat1 - lat2) / 2;
+        final double L = (lon1 - lon2) / 2;
 
         final double sing = Math.sin(G);
         final double cosl = Math.cos(L);
@@ -147,125 +184,113 @@ public final class QualifiedCoordinates implements GeodeticPosition {
         final double C = cosg * cosg * cosl * cosl + sinf * sinf * sinl * sinl;
         final double W = ExtraMath.atan2(Math.sqrt(S), Math.sqrt(C));
         final double R = Math.sqrt((S * C)) / W;
-        final double H1 = (3D * R - 1.0D) / (2.0D * C);
-        final double H2 = (3D * R + 1.0D) / (2.0D * S);
+        final double H1 = (3 * R - 1D) / (2 * C);
+        final double H2 = (3 * R + 1D) / (2 * S);
         final double D = 2 * W * 6378137;
-        return (float) (D * (1 + (1.0D / 298.257223563D) * H1 * sinf * sinf * cosg * cosg -
-            (1.0D / 298.257223563D) * H2 * cosf * cosf * sing * sing));
-
-        /*
-         * calculation for spherical model
-         */
-/*
-        final double R = Datum.DATUM_WGS_84.getEllipsoid().getEquatorialRadius();
-
-        final double lat1 = Math.toRadians(lat);
-        final double lon1 = Math.toRadians(lon);
-        final double lat2 = Math.toRadians(neighbourLat);
-        final double lon2 = Math.toRadians(neighbourLon);
-
-        final double temp_cosLat1 = Math.cos(lat1);
-        final double x1 = R * (temp_cosLat1 * Math.cos(lon1));
-        final double y1 = R * (Math.sin(lat1));
-        final double z1 = R * (temp_cosLat1 * Math.sin(lon1));
-        final double temp_cosLat2 = Math.cos(lat2);
-        final double x2 = R * (temp_cosLat2 * Math.cos(lon2));
-        final double y2 = R * (Math.sin(lat2));
-        final double z2 = R * (temp_cosLat2 * Math.sin(lon2));
-
-        final double dx = (x2 - x1);
-        final double dy = (y2 - y1);
-        final double dz = (z2 - z1);
-
-        return (float) Math.sqrt(dx * dx + dy * dy + dz * dz);
-*/
-
-        /*
-         * good results only for distances up to 10-15 km
-         */
-/*
-        final double dx = Math.abs(lon - neighbourLon) * (111319.490 * Math.cos(Math.toRadians((lat + neighbourLat) / 2)));
-        final double dy = Math.abs(lat - neighbourLat) * (111319.490);
-
-        return (float) Math.sqrt(dx * dx + dy * dy);
-*/
+        return (float) (D * (1 + (1D / 298.257223563) * H1 * sinf * sinf * cosg * cosg -
+            (1D / 298.257223563) * H2 * cosf * cosf * sing * sing));
     }
 
-    /* non-JSR179 signature TODO fix */
-    public float azimuthTo(final QualifiedCoordinates neighbour, final double distance) {
-        double result = 0D;
-
-        final int ilat1 = (int)(0.50D + lat * 360000D);
-        final int ilat2 = (int)(0.50D + neighbour.lat * 360000D);
-        final int ilon1 = (int)(0.50D + lon * 360000D);
-        final int ilon2 = (int)(0.50D + neighbour.lon * 360000D);
-
-        final double lat1 = Math.toRadians(lat);
-        final double lon1 = Math.toRadians(lon);
-        final double lat2 = Math.toRadians(neighbour.lat);
-        final double lon2 = Math.toRadians(neighbour.lon);
-
-        if (ilat1 == ilat2 && ilon1 == ilon2) {
-            return (float) result;
-        } else if (ilon1 == ilon2) {
-            if (ilat1 > ilat2) {
-                result = 180.0;
-            }
-        } else {
-            final double c = ExtraMath.acos(Math.sin(lat2) * Math.sin(lat1) + Math.cos(lat2) * Math.cos(lat1) * Math.cos((lon2 - lon1)));
-            final double A = ExtraMath.asin(Math.cos(lat2) * Math.sin((lon2 - lon1)) / Math.sin(c));
-            result = Math.toDegrees(A);
-
-            if ((ilat2 > ilat1) && (ilon2 > ilon1)) {
-            } else if ((ilat2 < ilat1) && (ilon2 < ilon1)){
-                result = 180.0 - result;
-            } else if ((ilat2 < ilat1) && (ilon2 > ilon1)) {
-                result = 180.0 - result;
-            } else if ((ilat2 > ilat1) && (ilon2 < ilon1)) {
-                result += 360.0;
-            }
-        }
-
-        return (float) result;
+    public static QualifiedCoordinates project(final QualifiedCoordinates from,
+                                               final double bearing, final double distance) {
 
         /*
-         * Pythagoras would be happy :-))))
+         * Free code from http://www.gavaghan.org/blog/free-source-code/geodesy-library-vincentys-formula-java/.
          */
-/*
-        final double dx = neighbour.lon - lon;
-        final double dy = neighbour.lat - lat;
-        double artificalLat, artificalLon;
-        final int offset;
 
-        if (dx > 0) {
-            if (dy > 0) {
-                artificalLat = lat;
-                artificalLon = neighbour.lon;
-                offset = 0;
-            } else {
-                artificalLat = neighbour.lat;
-                artificalLon = lon;
-                offset = 90;
-            }
-        } else {
-            if (dy > 0) {
-                artificalLat = neighbour.lat;
-                artificalLon = lon;
-                offset = 270;
-            } else {
-                artificalLat = lat;
-                artificalLon = neighbour.lon;
-                offset = 180;
-            }
+        final double f = 1D / 298.257223563D;
+        final double a = 6378137;
+        final double b = (1D - f) * a;
+        final double aSquared = a * a;
+        final double bSquared = b * b;
+        final double phi1 = Math.toRadians(from.getLat());
+        final double alpha1 = Math.toRadians(bearing);
+        final double cosAlpha1 = Math.cos(alpha1);
+        final double sinAlpha1 = Math.sin(alpha1);
+        final double tanU1 = (1D - f) * Math.tan(phi1);
+        final double cosU1 = 1D / Math.sqrt(1D + tanU1 * tanU1);
+        final double sinU1 = tanU1 * cosU1;
+
+        // eq. 1
+        final double sigma1 = ExtraMath.atan2(tanU1, cosAlpha1);
+
+        // eq. 2
+        final double sinAlpha = cosU1 * sinAlpha1;
+        final double sin2Alpha = sinAlpha * sinAlpha;
+        final double cos2Alpha = 1D - sin2Alpha;
+        final double uSquared = cos2Alpha * (aSquared - bSquared) / bSquared;
+
+        // eq. 3
+        final double A = 1 + (uSquared / 16384) * (4096 + uSquared * (-768 + uSquared * (320 - 175 * uSquared)));
+
+        // eq. 4
+        final double B = (uSquared / 1024) * (256 + uSquared * (-128 + uSquared * (74 - 47 * uSquared)));
+
+        // iterate until there is a negligible change in sigma
+        double deltaSigma;
+        final double sOverbA = distance / (b * A);
+        double sigma = sOverbA;
+        double sinSigma;
+        double prevSigma = sOverbA;
+        double sigmaM2;
+        double cosSigmaM2;
+        double cos2SigmaM2;
+
+        for (; ;) {
+            // eq. 5
+            sigmaM2 = 2 * sigma1 + sigma;
+            cosSigmaM2 = Math.cos(sigmaM2);
+            cos2SigmaM2 = cosSigmaM2 * cosSigmaM2;
+            sinSigma = Math.sin(sigma);
+            final double cosSignma = Math.cos(sigma);
+
+            // eq. 6
+            deltaSigma = B
+                    * sinSigma
+                    * (cosSigmaM2 + (B / 4)
+                    * (cosSignma * (-1 + 2 * cos2SigmaM2) - (B / 6) * cosSigmaM2 * (-3 + 4 * sinSigma * sinSigma) * (-3 + 4 * cos2SigmaM2)));
+
+            // eq. 7
+            sigma = sOverbA + deltaSigma;
+
+            // break after converging to tolerance
+            if (Math.abs(sigma - prevSigma) < 0.0000000000001)
+                break;
+
+            prevSigma = sigma;
         }
 
-        final double a = distance(artificalLat, artificalLon);
-        double sina = a / distance;
-        if (sina > 1.0D && sina < 1.001D) { // tolerate calculation inaccuracy 
-            sina = 1.0D;
-        }
+        sigmaM2 = 2 * sigma1 + sigma;
+        cosSigmaM2 = Math.cos(sigmaM2);
+        cos2SigmaM2 = cosSigmaM2 * cosSigmaM2;
 
-        return (float) (offset + Math.toDegrees(ExtraMath.asin(sina)));
-*/
+        final double cosSigma = Math.cos(sigma);
+        sinSigma = Math.sin(sigma);
+
+        // eq. 8
+        final double phi2 = ExtraMath.atan2(sinU1 * cosSigma + cosU1 * sinSigma * cosAlpha1, (1D - f)
+                * Math.sqrt(sin2Alpha + ExtraMath.powi(sinU1 * sinSigma - cosU1 * cosSigma * cosAlpha1, 2)));
+
+        // eq. 9
+        // This fixes the pole crossing defect spotted by Matt Feemster. When a
+        // path passes a pole and essentially crosses a line of latitude twice -
+        // once in each direction - the longitude calculation got messed up. Using
+        // atan2 instead of atan fixes the defect. The change is in the next 3
+        // lines.
+        // double tanLambda = sinSigma * sinAlpha1 / (cosU1 * cosSigma - sinU1 * sinSigma * cosAlpha1);
+        // double lambda = Math.atan(tanLambda);
+        final double lambda = ExtraMath.atan2(sinSigma * sinAlpha1, (cosU1 * cosSigma - sinU1 * sinSigma * cosAlpha1));
+
+        // eq. 10
+        final double C = (f / 16) * cos2Alpha * (4 + f * (4 - 3 * cos2Alpha));
+
+        // eq. 11
+        final double L = lambda - (1D - C) * f * sinAlpha * (sigma + C * sinSigma * (cosSigmaM2 + C * cosSigma * (-1 + 2 * cos2SigmaM2)));
+
+        // result
+        final double latitude = Math.toDegrees(phi2);
+        final double longitude = from.getLon() + Math.toDegrees(L);
+
+        return newInstance(latitude, longitude);
     }
 }

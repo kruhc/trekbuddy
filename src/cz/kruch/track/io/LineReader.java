faca58a3d2241348d79611dceb1a3aeba38b7b68
kruch <kruhc@seznam.cz>
2011-08-05 10:49:27 +0000

    removed dead code

--- src/cz/kruch/track/io/LineReader.java
+++ src/cz/kruch/track/io/LineReader.java
@@ -11,7 +11,7 @@ import java.io.InputStreamReader;
 /**
  * Line reading class for text files (calibration files, NMEA logs, ...).
  *
- * @author Ales Pour <kruhc@seznam.cz>
+ * @author kruhc@seznam.cz
  */
 public final class LineReader extends InputStreamReader {
 //#ifdef __LOG__
@@ -23,43 +23,12 @@ public final class LineReader extends InputStreamReader {
     private static final int MAX_LINE_LENGTH    = 256;
     private static final int DEFAULT_BUFF_SIZE  = 1024;
 
-/*
-    private static InfiniteInputStream iin;
-    private static InputStreamReader iir;
-*/
-
     private CharArrayTokenizer.Token token;
     private char[] buffer;
     private int count;
     private int position;
 
-/*
-    private static void ensureReader(InputStream in) {
-//#ifdef __LOG__
-        if (log.isEnabled()) log.debug("get reader for " + in);
-//#endif
-
-        if (iin == null) {
-            iin = new InfiniteInputStream(in);
-        } else {
-            iin.use(in);
-        }
-        if (iir == null) {
-            iir = new InputStreamReader(iin);
-        } else {
-            try {
-                iir.reset();
-            } catch (IOException e) {
-                // ignore - reader does not support reset
-            }
-        }
-    }
-*/
-
     public LineReader(InputStream in, int buffSize) {
-/*
-        ensureReader(in);
-*/
         super(in);
         this.token = new CharArrayTokenizer.Token();
         this.buffer = new char[buffSize];
@@ -69,12 +38,6 @@ public final class LineReader extends InputStreamReader {
     public LineReader(InputStream in) {
         this(in, DEFAULT_BUFF_SIZE);
     }
-
-/*
-    public void close() throws IOException {
-        iin.close();
-    }
-*/
 
     public String readLine(boolean ignoreLF) throws IOException {
         final CharArrayTokenizer.Token result = readToken(ignoreLF);
@@ -119,6 +82,8 @@ public final class LineReader extends InputStreamReader {
                     } else {
                         break;
                     }
+//                } else if (_count == 0) {
+//                    throw new RuntimeException("reader.read([])=0");
                 } else {
                     this.count += _count;
                     count += _count;
@@ -152,62 +117,4 @@ public final class LineReader extends InputStreamReader {
 
         throw new IllegalStateException("Line length > " + MAX_LINE_LENGTH);
     }
-
-/*
-    static final class InfiniteInputStream extends InputStream {
-
-        private InputStream in;
-
-        public InfiniteInputStream(InputStream in) {
-            this.in = in;
-        }
-
-        public void use(InputStream in) throws IllegalStateException {
-            if (this.in != null) {
-                throw new IllegalStateException("Stream already in use");
-            }
-            this.in = in;
-        }
-
-        public int read() throws IOException {
-            return in.read();
-        }
-
-        public int read(byte[] b) throws IOException {
-            return in.read(b);
-        }
-
-        public int read(byte[] b, int off, int len) throws IOException {
-            return in.read(b, off, len);
-        }
-
-        public long skip(long n) throws IOException {
-            return in.skip(n);
-        }
-
-        public int available() throws IOException {
-            return in.available();
-        }
-
-        public synchronized void mark(int readlimit) {
-            in.mark(readlimit);
-        }
-
-        public synchronized void reset() throws IOException {
-            if (in == null) { // situation: called by reader when getting prepared for reuse
-                return;
-            }
-            in.reset();
-        }
-
-        public boolean markSupported() {
-            return in.markSupported();
-        }
-
-        public void close() throws IOException {
-            in.close();
-            in = null; // gc hint
-        }
-    }
-*/
 }

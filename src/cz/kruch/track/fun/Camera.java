8dbcc58e89808c5f7ffb7a8497c201f85b6d401f
kruch <kruhc@seznam.cz>
2012-01-17 18:43:10 +0000

    remove unused import

--- src/cz/kruch/track/fun/Camera.java
+++ src/cz/kruch/track/fun/Camera.java
@@ -6,7 +6,6 @@ import cz.kruch.track.event.Callback;
 import cz.kruch.track.configuration.Config;
 import cz.kruch.track.ui.Desktop;
 import cz.kruch.track.Resources;
-import cz.kruch.track.util.Worker;
 
 //#ifndef __ANDROID__
 import javax.microedition.media.Player;

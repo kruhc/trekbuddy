f77b1bbe6e4dde017b1da7897a53bc2023a426c9
kruch <kruhc@seznam.cz>
2012-12-10 21:08:20 +0000

    links memory optimization and querying improvements

--- src/cz/kruch/track/location/ExtWaypoint.java
+++ src/cz/kruch/track/location/ExtWaypoint.java
@@ -6,12 +6,14 @@ import api.location.QualifiedCoordinates;
 
 import java.util.Vector;
 
+import cz.kruch.track.util.NakedVector;
+
 /**
- * Extende waypoint with links and groundspeak extensions support.
+ * Extended waypoint with links and groundspeak extensions support.
  */
 public class ExtWaypoint extends StampedWaypoint {
     private Object userObject;
-    private Vector links;
+    private /*Vector*/Object links;
 
     public ExtWaypoint(QualifiedCoordinates qc, String name, String comment, long timestamp) {
         super(qc, name, comment, timestamp);
@@ -21,7 +23,13 @@ public class ExtWaypoint extends StampedWaypoint {
                        long timestamp, GroundspeakBean bean, Vector links) {
         super(qc, name, comment, sym, timestamp);
         this.userObject = bean;
-        this.links = links;
+        if (links != null && links.size() != 0) {
+            if (links.size() == 1) {
+                this.links = links.elementAt(0);
+            } else {
+                this.links = new NakedVector((NakedVector) links);
+            }
+        }
     }
 
     public Object getUserObject() {
@@ -29,31 +37,41 @@ public class ExtWaypoint extends StampedWaypoint {
     }
 
     public Vector getLinks() {
-        return links;
+        if (links == null || links instanceof Vector) {
+            return (Vector) links;
+        }
+        final Vector v = new Vector(1);
+        v.addElement(links);
+        return v;
     }
 
     public void addLink(String link) {
         if (this.links == null) {
-            this.links = new Vector(4, 4);
+            this.links = link;
+        } else {
+            if (this.links instanceof String) {
+                final String s = (String) this.links;
+                this.links = new Vector(2, 2);
+                ((Vector) this.links).addElement(s);
+            }
+            ((Vector) this.links).addElement(link);
         }
-        this.links.addElement(link);
     }
 
     public String getLink(int type) {
         if (links != null) {
-            for (int N = links.size(), i = 0; i < N; i++) {
-                final String link = (String) links.elementAt(i);
-                switch (type) {
-                    case LINK_GENERIC_IMAGE: {
-                        if (link.endsWith(".jpg") || link.endsWith(".png")) {
-                            return link;
-                        }
-                    } break;
-                    case LINK_GENERIC_SOUND: {
-                        if (link.endsWith(".amr") || link.endsWith(".wav") || link.endsWith(".mp3") || link.endsWith(".aac") || link.endsWith(".m4a") || link.endsWith(".3gp")) {
-                            return link;
-                        }
-                    } break;
+            if (links instanceof String) {
+                String link = (String) this.links;
+                if (isTypedLink(link, type)) {
+                    return link;
+                }
+            } else {
+                final Vector links = (Vector) this.links;
+                for (int N = links.size(), i = 0; i < N; i++) {
+                    final String link = (String) links.elementAt(i);
+                    if (isTypedLink(link, type)) {
+                        return link;
+                    }
                 }
             }
         }
@@ -66,5 +84,18 @@ public class ExtWaypoint extends StampedWaypoint {
             return super.toString();
         }
         return userObject.toString();
+    }
+
+    private static boolean isTypedLink(final String link, final int type) {
+        boolean result = false;
+        switch (type) {
+            case LINK_GENERIC_IMAGE: {
+                result = link.endsWith(".jpg") || link.endsWith(".png");
+            } break;
+            case LINK_GENERIC_SOUND: {
+                result = link.endsWith(".amr") || link.endsWith(".wav") || link.endsWith(".mp3") || link.endsWith(".aac") || link.endsWith(".m4a") || link.endsWith(".3gp");
+            } break;
+        }
+        return result;
     }
 }

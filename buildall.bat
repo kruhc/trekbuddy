731ddd957b11755a6f9f44e75a6be05fcd92f2f9
kruch <kruhc@seznam.cz>
2012-12-17 09:10:57 +0000

    using build versioning

--- buildall.bat
+++ buildall.bat
@@ -1,42 +1,53 @@
 @echo off
 
+if "%1"=="" goto help
+if not exist properties\build-%1.properties goto vendor
+if "%ANT_HOME%"=="" set ANT_HOME=z:\apps\apache-ant-1.8.2
+
 set RESOURCE_LANG=en
 echo Using locale '%RESOURCE_LANG%'
 
-if "%1"=="" goto help
-if not exist properties\build-%1.properties goto vendor
+java -cp tools/classes;%ANT_HOME%/lib/ant.jar BuildVersion public
 
 echo Build version 'standard' ...
-cmd /c build %1 standard %RESOURCE_LANG%
+call :build %1 standard %RESOURCE_LANG%
 
 echo Build version 'standard lite' ...
-cmd /c build %1 standard-lite %RESOURCE_LANG%
+call :build %1 standard-lite %RESOURCE_LANG%
 
 echo Build version 'symbian' ...
-cmd /c build %1 symbian %RESOURCE_LANG%
+call :build %1 symbian %RESOURCE_LANG%
 
 echo Build version 'basic' ...
-cmd /c build %1 basic %RESOURCE_LANG%
+call :build %1 basic %RESOURCE_LANG%
 
 echo Build version 'j9' ...
-cmd /c build %1 j9 %RESOURCE_LANG%
+call :build %1 j9 %RESOURCE_LANG%
 
-rem echo Build version 'rim41' ...
-rem cmd /c build %1 rim41 %RESOURCE_LANG%
+REM echo Build version 'rim41' ...
+REM call :build %1 rim41 %RESOURCE_LANG%
 
 echo Build version 'rim42' ...
-cmd /c build %1 rim42 %RESOURCE_LANG%
+call :build %1 rim42 %RESOURCE_LANG%
 
 echo Build version 'rim50' ...
-cmd /c build %1 rim50 %RESOURCE_LANG%
+call :build %1 rim50 %RESOURCE_LANG%
 
 echo Build version 'android' ...
-cmd /c build %1 android %RESOURCE_LANG%
+call :build %1 android %RESOURCE_LANG%
 
 echo Build version 'android-backport' ...
-cmd /c build %1 android-backport %RESOURCE_LANG%
+call :build %1 android-backport %RESOURCE_LANG%
+
+echo Build version 'playbook' ...
+echo COMMENTED OUT: call :build %1 playbook.signed %RESOURCE_LANG%
 
 goto end
+
+:build
+cmd /c %ANT_HOME%\bin\ant -Dinclude.properties=properties\build-%1.properties -Dlocale=%RESOURCE_LANG% -l dist\%1\standard\build.log -f build.xml %2
+echo.
+exit /b
 
 :vendor
 echo No such vendor - %1

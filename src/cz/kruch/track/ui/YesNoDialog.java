25e28b7f9351e038c7709bdaab79ad70d0e7f090
kruch <kruhc@seznam.cz>
2012-12-20 10:40:00 +0000

    removed unused import

--- src/cz/kruch/track/ui/YesNoDialog.java
+++ src/cz/kruch/track/ui/YesNoDialog.java
@@ -8,7 +8,6 @@ import javax.microedition.lcdui.Displayable;
 import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Alert;
-import javax.microedition.lcdui.AlertType;
 import javax.microedition.lcdui.TextBox;
 import javax.microedition.lcdui.TextField;
 

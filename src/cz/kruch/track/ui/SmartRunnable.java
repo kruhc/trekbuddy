982db25fd61ed49080fbc43c412934a1f941866d
kruch <kruhc@seznam.cz>
2013-07-07 06:57:26 +0000

    added thread-based executor for CN1; more detailed logging

--- src/cz/kruch/track/ui/SmartRunnable.java
+++ src/cz/kruch/track/ui/SmartRunnable.java
@@ -4,6 +4,10 @@ package cz.kruch.track.ui;
 
 import java.util.Vector;
 
+//#ifdef __CN1__
+//#define __EXECUTOR__
+//#endif
+
 /**
  * Buffered backend for Display.callserially(Runnable) with task merging support.
  * 
@@ -13,6 +17,10 @@ import java.util.Vector;
  * @author kruhc@seznam.cz
  */
 final class SmartRunnable implements Runnable {
+//#ifdef __LOG__
+    private static final cz.kruch.track.util.Logger log = new cz.kruch.track.util.Logger("SmartRunnable");
+//#endif
+
     // statistics
     static int uncaught, mergedRT, mergedKT, maxQT;
 
@@ -25,6 +33,26 @@ final class SmartRunnable implements Runnable {
     SmartRunnable() {
         this.runnables = new Vector(16);
         this.active = true;
+//#ifdef __EXECUTOR__
+        final Thread executor = new Thread("TrekBuddy [CallSerially]") {
+            public void run() {
+                while (true) {
+                    synchronized (SmartRunnable.this) {
+                        while (runnables.size() == 0) {
+                            try {
+                                SmartRunnable.this.wait();
+                            } catch (InterruptedException e) {
+                                // ignore
+                            }
+                        }
+                    }
+                    SmartRunnable.this.run();
+                }
+            }
+        };
+        executor.setPriority(Thread.MAX_PRIORITY);
+        executor.start();
+//#endif
     }
 
     void setActive(boolean active) {
@@ -64,15 +92,24 @@ final class SmartRunnable implements Runnable {
             // try task merge first
             if (count > 0) {
                 final Object last = runnables.lastElement();
+//#ifdef __LOG__
+                if (log.isEnabled()) log.debug("merge? new: " + r + "; stack: " + last);
+//#endif
                 if (r instanceof Desktop.RenderTask) { // trick #1: merge render tasks
                     if (last instanceof Desktop.RenderTask) {
                         ((Desktop.RenderTask) last).merge(((Desktop.RenderTask) r));
                         mergedRT++;
+//#ifdef __LOG__
+                        if (log.isEnabled()) log.debug("render task merged");
+//#endif
                         return;
                     }
                 } else if (r instanceof DeviceScreen) { // trick #2: avoid duplicates of key-hold checks
                     if (last instanceof DeviceScreen) {
                         mergedKT++;
+//#ifdef __LOG__
+                        if (log.isEnabled()) log.debug("devicescreen task merged");
+//#endif
                         return;
                     }
                 }
@@ -81,6 +118,9 @@ final class SmartRunnable implements Runnable {
             // no task to merge with, just append
             runnables.addElement(r);
             count++;
+//#ifdef __LOG__
+            if (log.isEnabled()) log.debug("task queued; count = " + count);
+//#endif
 
             // debug info
             if (count > maxQT) {
@@ -96,6 +136,9 @@ final class SmartRunnable implements Runnable {
         }
 
         // better enqueue it out of synchronized block
+//#ifdef __LOG__
+        if (log.isEnabled()) log.debug("fire task? " + fire);
+//#endif
         if (fire) {
             execute();
         }
@@ -124,6 +167,9 @@ final class SmartRunnable implements Runnable {
 
         // execute
         if (r != null) {
+//#ifdef __LOG__
+            if (log.isEnabled()) log.debug("running task " + r);
+//#endif
             try {
                 r.run();
             } catch (Throwable t) {
@@ -132,6 +178,9 @@ final class SmartRunnable implements Runnable {
                 t.printStackTrace();
 //#endif
             }
+//#ifdef __LOG__
+            if (log.isEnabled()) log.debug("task " + r + " finished");
+//#endif
         }
 
         // fire flag
@@ -147,12 +196,22 @@ final class SmartRunnable implements Runnable {
         }
 
         // better enqueue it out of synchronized block
+//#ifdef __LOG__
+        if (log.isEnabled()) log.debug("run another? " + fire);
+//#endif
         if (fire) {
             execute();
         }
     }
 
     private void execute() {
+//#ifndef __EXECUTOR__
         Desktop.display.callSerially(this);
+//#else
+        synchronized (this) {
+            this.notify();
+        }
+        Thread.yield();
+//#endif
     }
 }

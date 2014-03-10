9771a79f84c645757e48f90c21704d6d9c33c8d3
kruch <kruhc@seznam.cz>
2014-03-10 20:25:10 +0000

    Sonim backlight

---
+++ src/cz/kruch/track/ui/nokia/SonimDeviceControl.java
@@ -0,0 +1,30 @@
+// @LICENSE@
+
+package cz.kruch.track.ui.nokia;
+
+/**
+ * Device control implementation for Sonim phones.
+ *
+ * @author Ales Pour <kruhc@seznam.cz>
+ */
+final class SonimDeviceControl extends DeviceControl {
+
+    private com.sonimtech.j2me.BacklightController controller;
+
+    SonimDeviceControl() {
+        this.name = "Sonim";
+    }
+
+    /** @overriden */
+    void turnOn() {
+        controller = new com.sonimtech.j2me.BacklightController();
+        controller.turnOnBacklight(com.sonimtech.j2me.BacklightController.BACKLIGHTPERMANENT);
+    }
+
+    /** @overriden */
+    void turnOff() {
+        controller.closeBacklight();
+        controller = null;
+    }
+}
+

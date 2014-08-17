8179c107476b8db74174b6d13ab9148bd710e918
kruch <kruhc@seznam.cz>
2014-08-17 08:36:25 +0000

    extended Graphics for WP

---
+++ cn1-compat/src/javax/microedition/lcdui/game/ExtendedGraphics.java
@@ -0,0 +1,8 @@
+package javax.microedition.lcdui.game;
+
+public interface ExtendedGraphics {
+    public void setFlushable(boolean flushable);
+    public void reset();
+    public void flush();
+    public void realize();
+}

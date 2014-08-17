30cfa65da6f3489e7b568e69d3b1d90bae938075
kruch <kruhc@seznam.cz>
2014-08-17 08:34:53 +0000

    added skeleton for .NET ThreadPool use in WP build

---
+++ src/api/lang/ThreadPool.java
@@ -0,0 +1,7 @@
+package api.lang;
+
+public final class ThreadPool {
+    public static boolean QueueUserWorkItem(Runnable runnable) {
+        throw new java.lang.RuntimeException("override");
+    }
+}

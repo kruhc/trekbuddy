f244abcf0a8c7943c4ab0f02ce8b8413689fa70c
kruch <kruhc@seznam.cz>
2014-09-06 08:26:24 +0000

    microoptimization

--- src/api/io/RandomAccessInputStream.java
+++ src/api/io/RandomAccessInputStream.java
@@ -22,7 +22,7 @@ public final class RandomAccessInputStream extends InputStream {
     }
 
     public int read(byte[] buffer) throws IOException {
-        return file.read(buffer);
+        return file.read(buffer, 0, buffer.length);
     }
 
     public int read(byte[] buffer, int offset, int length) throws IOException {

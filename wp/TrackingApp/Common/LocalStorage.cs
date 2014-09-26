da68e7fcc194ae3da345d44e84e6551e88911f6d
kruch <kruhc@seznam.cz>
2014-09-26 12:32:36 +0000

    added Delete methods

--- wp/TrackingApp/Common/LocalStorage.cs
+++ wp/TrackingApp/Common/LocalStorage.cs
@@ -64,6 +64,41 @@ namespace net.trekbuddy.wp8
             return result;
         }
 
+        public static void DeleteFile(string path)
+        {
+            using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                store.DeleteFile(path);
+            }
+        }
+
+        public static void DeleteFolder(string path)
+        {
+#if LOG
+            CN1Extensions.Log("LocalStorage.deleteFolder: {0}", path);
+#endif
+            using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                foreach (string fileName in store.GetFileNames(path + "/*"))
+                {
+                    string target = string.Format("{0}/{1}", path, fileName);
+#if LOG
+                    CN1Extensions.Log("LocalStorage.deleteFolder: delete file {0}", target);
+#endif
+                    store.DeleteFile(target);
+                }
+                foreach (string dirName in store.GetDirectoryNames(path + "/*"))
+                {
+                    string target = string.Format("{0}/{1}", path, dirName);
+#if LOG
+                    CN1Extensions.Log("LocalStorage.deleteFolder: delete sub-folder {0}", target);
+#endif
+                    DeleteFolder(target);
+                }
+                store.DeleteDirectory(path);
+            }
+        }
+
         public static Stream OpenInputStream(string path)
         {
 #if LOG

62f9b885281b50f18a6312e5d1610567132a6cea
kruch <kruhc@seznam.cz>
2015-02-10 11:31:47 +0000

    improved waiting methods (using ConfigureAwait and GetResult)

--- wp/TrackingApp/Common/NETExtensions.cs
+++ wp/TrackingApp/Common/NETExtensions.cs
@@ -8,6 +8,9 @@ namespace TrackingApp
 {
     internal static class NETExtensions
     {
+
+#if false
+
         [MethodImplAttribute(MethodImplOptions.NoInlining)]
         public static void SafeWait(this Task task, string failureMessage = null)
         {
@@ -45,5 +48,58 @@ namespace TrackingApp
             //throw new global::org.xmlvm._nExceptionAdapter(e.ToJavaException());
             return e;
         }
+
+#else
+
+        [MethodImplAttribute(MethodImplOptions.NoInlining)]
+        public static void SafeWait(this Task task, string failureMessage = null)
+        {
+            try
+            {
+                task.ConfigureAwait(false).GetAwaiter().GetResult();
+            }
+            catch (Exception e)
+            {
+                throw Throw(e, failureMessage);
+            }
+        }
+
+        [MethodImplAttribute(MethodImplOptions.NoInlining)]
+        public static T SafeWait<T>(this Task<T> task, string failureMessage = null)
+        {
+            try
+            {
+                return task.ConfigureAwait(false).GetAwaiter().GetResult();
+            }
+            catch (Exception e)
+            {
+                throw Throw(e, failureMessage);
+            }
+        }
+
+        [MethodImplAttribute(MethodImplOptions.NoInlining)]
+        public static void FastWait(this Task task)
+        {
+            task.ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        [MethodImplAttribute(MethodImplOptions.NoInlining)]
+        public static T FastWait<T>(this Task<T> task)
+        {
+            return task.ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        private static Exception Throw(Exception e, string failureMessage = null)
+        {
+            if (failureMessage == null)
+                CN1Extensions.Log(e.ToString(), CN1Extensions.Level.ERROR);
+            else
+                CN1Extensions.Log((failureMessage + e.ToString()), CN1Extensions.Level.ERROR);
+            //throw new global::org.xmlvm._nExceptionAdapter(e.ToJavaException());
+            return e;
+        }
+
+#endif
+
     }
 }

56dc068a1a8a63ea5273746c79af91cb1c273244
kruch <kruhc@seznam.cz>
2017-03-01 11:22:56 +0000

    unfinished logger

--- src/cz/kruch/track/util/Logger.java
+++ src/cz/kruch/track/util/Logger.java
@@ -9,97 +9,68 @@ package cz.kruch.track.util;
  *
  * @author kruhc@seznam.cz
  */
-public final class Logger {
-    private static final String LEVEL_DEBUG  = "DEBUG";
-    private static final String LEVEL_INFO   = "INFO";
-    private static final String LEVEL_WARN   = "WARN";
-    private static final String LEVEL_ERROR  = "ERROR";
+public final class Logger /*extends net.trekbuddy.util.Logger*/ {
 
-    private String cname;
-    private boolean enabled;
+    private static boolean enabled;
 
     public static void out(String message) {
 //#ifdef __ANDROID__
         android.util.Log.i("TrekBuddy", message);
 //#elifdef __CN1__
-        com.codename1.io.Log.p(message, com.codename1.io.Log.INFO);
+        // no out
 //#else
         System.out.println(message);
 //#endif
     }
 
-    public static void printStackTrace(Throwable t) {
-//#ifndef __CN1__
-        t.printStackTrace();
+    public static void out(Throwable t) {
+//#ifdef __ANDROID__
+        android.util.Log.e("TrekBuddy", null, t);
+//#elifdef __CN1__
+        // no out
 //#else
-        com.codename1.io.Log.e(t);
+        t.printStackTrace();
 //#endif
     }
 
-    public Logger(String componentName) {
-        this.cname = componentName;
-        this.enabled = cz.kruch.track.TrackingMIDlet.isLogEnabled();
+    public static void setEnabled(boolean enabled) {
+//#ifndef __CN1__
+        Logger.enabled = enabled;
+//#endif
+    }
+
+    public Logger(String category) {
+//        super(category);
     }
 
     public boolean isEnabled() { // effectively isDebugEnabled()
 //#ifndef __CN1__
         return enabled;
 //#else
-        return com.codename1.io.Log.getLevel() == com.codename1.io.Log.DEBUG; 
+        return net.trekbuddy.util.Logger.getLevel() == net.trekbuddy.util.Logger.DEBUG;
 //#endif
     }
 
     public void debug(String message) {
-        log(LEVEL_DEBUG, message);
+        out(message);
     }
 
-    public void debug(String message, Throwable t) {
-        log(LEVEL_DEBUG, message);
-        log(t);
-    }
-
-    public void info(String message) {
-        log(LEVEL_INFO, message);
+    public void debug(String message, Throwable throwable) {
+        out(message);
+        out(throwable);
     }
 
     public void warn(String message) {
-        log(LEVEL_WARN, message);
-    }
-
-    public void warn(String message, Throwable t) {
-        log(LEVEL_WARN, message);
-        log(t);
+        out(message);
     }
 
     public void error(String message) {
-        log(LEVEL_ERROR, message);
+        out(message);
     }
 
-    public void error(String message, Throwable t) {
-        log(LEVEL_ERROR, message);
-        log(t);
-    }
-
-    private void log(String severity, String message) {
-        if (enabled) {
-//#ifndef __CN1__
-            System.out.println("[" + (new java.util.Date()) + "] " + cname + " - " + message);
-            System.out.flush();
-//#else
-            com.codename1.io.Log.p(cname + " - " + message);
-//#endif
-        }
-    }
-
-    private void log(Throwable t) {
-        if (enabled) {
-//#ifndef __CN1__
-            t.printStackTrace();
-            System.err.flush();
-//#else
-            com.codename1.io.Log.e(t);
-//#endif
-        }
+    public void error(String message, Throwable throwable) {
+        out(message);
+        out(throwable);
     }
 }
 

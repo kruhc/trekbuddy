3f7c187cc32312d384cd7cbad26aa98188aa1c2c
kruhc <kruhc@seznam.cz>
2019-09-07 17:43:27 +0200

    removed obsolete option

--- build.bat
+++ build.bat
@@ -4,17 +4,20 @@ setlocal
 
 if "%1"=="" goto help
 if "%2"=="" goto help
-if "%3"=="" goto help
 if not exist properties\build-%1.properties goto vendor
 
 if "%ANT_HOME%"=="" set ANT_HOME=z:\apps\apache-ant-1.8.2
 
 rem override
-set JAVA_HOME=c:\Program Files (x86)\Java\jdk1.7.0_71
+rem set JAVA_HOME=c:\Program Files (x86)\Java\jdk1.7.0_71
+set JAVA_HOME=c:\Program Files (x86)\Java\jdk1.8.0_144
 
 java -cp tools/classes;%ANT_HOME%/lib/ant.jar BuildVersion public
 
-cmd /c %ANT_HOME%\bin\ant -Dinclude.properties=properties\build-%1.properties -Dlocale=%3 -l dist\%1\%2\build.log -f build.xml %2
+mkdir dist\%1\%2 2>NUL 
+
+cmd /c %ANT_HOME%\bin\ant -Dinclude.properties=properties\build-%1.properties -l dist\%1\%2\build.log -f build.xml %2
+
 goto end
 
 :vendor
@@ -22,9 +25,9 @@ echo No such vendor - %1
 goto end
 
 :help
-echo Usage: %0 vendor device locale
+echo Usage: %0 vendor device
 echo Example:
-echo        %0 public generic en_US
+echo        %0 public generic
 goto end
 
 :end
